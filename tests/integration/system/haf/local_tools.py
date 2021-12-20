08041b825ebf1c983e3372e2d4382b099a2f56a2
Marcin Sobczyk <msobczyk@syncad.com>
2021-12-20 19:50:45 +0100

    additional system tests

--- tests/integration/system/haf/local_tools.py
+++ tests/integration/system/haf/local_tools.py
@@ -1,53 +1,15 @@
-import json
-
-from test_tools import logger, Wallet, BlockLog
 from datetime import datetime, timezone
+import time
+import json
+from threading import Thread
+
+from test_tools import logger, Wallet, BlockLog, Account, Asset
+from witnesses import alpha_witness_names, beta_witness_names
+from tables import *
 
 
 BLOCKS_IN_FORK = 5
-BLOCKS_AFTER_FORK = 5
-
-
-def make_fork(world, main_chain_trxs=[], fork_chain_trxs=[]):
-    alpha_net = world.network('Alpha')
-    beta_net = world.network('Beta')
-    alpha_witness_node = alpha_net.node('WitnessNode0')
-    beta_witness_node = beta_net.node(name='WitnessNode0')
-
-    logger.info(f'Making fork at block {get_head_block(alpha_witness_node)}')
-
-    main_chain_wallet = Wallet(attach_to=alpha_witness_node)
-    fork_chain_wallet = Wallet(attach_to=beta_witness_node)
-    fork_block = get_head_block(beta_witness_node)
-    head_block = fork_block
-    alpha_net.disconnect_from(beta_net)
-
-    for trx in main_chain_trxs:
-        main_chain_wallet.api.sign_transaction(trx)
-    for trx in fork_chain_trxs:
-        fork_chain_wallet.api.sign_transaction(trx)
-
-    for node in [alpha_witness_node, beta_witness_node]:
-        node.wait_for_block_with_number(head_block + BLOCKS_IN_FORK)
-    alpha_net.connect_with(beta_net)
-    for node in [alpha_witness_node, beta_witness_node]:
-        node.wait_for_block_with_number(head_block + BLOCKS_IN_FORK + BLOCKS_AFTER_FORK)
-
-    head_block = get_head_block(beta_witness_node)
-    return head_block
-
-
-def wait_for_irreversible_progress(node, block_num):
-    logger.info(f'Waiting for progress of irreversible block')
-    head_block = get_head_block(node)
-    irreversible_block = get_irreversible_block(node)
-    logger.info(f"Current head_block_number: {head_block}, irreversible_block_num: {irreversible_block}")
-    while irreversible_block < block_num:
-        node.wait_for_block_with_number(head_block+1)
-        head_block = get_head_block(node)
-        irreversible_block = get_irreversible_block(node)
-        logger.info(f"Current head_block_number: {head_block}, irreversible_block_num: {irreversible_block}")
-    return irreversible_block, head_block
+WAIT_FOR_FORK_TIMEOUT = 90.0
 
 
 def get_head_block(node):
@@ -58,6 +20,92 @@ def get_head_block(node):
 def get_irreversible_block(node):
     irreversible_block_num = node.api.database.get_dynamic_global_properties()["last_irreversible_block_num"]
     return irreversible_block_num
+
+
+def make_fork(world, at_block=None, main_chain_trxs=[], fork_chain_trxs=[]):
+    alpha_net = world.network('Alpha')
+    beta_net = world.network('Beta')
+    alpha_witness_node = alpha_net.node('WitnessNode0')
+    beta_witness_node = beta_net.node(name='WitnessNode0')
+    node_under_test = beta_net.node(name='NodeUnderTest')
+
+    main_chain_wallet = Wallet(attach_to=world.network('Alpha').node('WitnessNode0'))
+    import_witnesses_keys(main_chain_wallet)
+    fork_chain_wallet = Wallet(attach_to=world.network('Beta').node('WitnessNode0'))
+    import_witnesses_keys(fork_chain_wallet)
+
+    fork_block = at_block or get_head_block(node_under_test)
+    logger.info(f'Making fork at block {fork_block}')
+    if at_block is not None:
+        node_under_test.wait_for_block_with_number(at_block)
+
+    alpha_net.disconnect_from(beta_net)
+
+    send_transactions_asynchronously(main_chain_wallet, main_chain_trxs)
+    send_transactions_asynchronously(fork_chain_wallet, fork_chain_trxs)
+
+    for node in [alpha_witness_node, beta_witness_node]:
+        node.wait_for_block_with_number(fork_block + BLOCKS_IN_FORK)
+    return fork_block
+
+
+def send_transactions_asynchronously(wallet, trxs):
+    def sign_and_send():
+        for trx in trxs:
+            wallet.api.sign_transaction(trx)
+    Thread(target=sign_and_send, daemon=True).start()
+
+
+def back_from_fork(world):
+    alpha_net = world.network('Alpha')
+    beta_net = world.network('Beta')
+    node_under_test = world.network('Beta').node('NodeUnderTest')
+
+    logger.info(f'Reconnecting forks')
+
+    alpha_net.connect_with(beta_net)
+    waited = wait_for_back_from_fork(node_under_test)
+    logger.info(f'Switching to different fork detected after {waited} seconds')
+
+    head_block = get_head_block(node_under_test)
+    return head_block
+
+
+def wait_for_back_from_fork(node, timeout=WAIT_FOR_FORK_TIMEOUT):
+    already_waited = 0
+    while not switched_fork(node):
+        if timeout - already_waited <= 0:
+            raise TimeoutError('Waited too long for switching forks')
+
+        sleep_time = min(1.0, timeout)
+        time.sleep(sleep_time)
+        already_waited += sleep_time
+
+    return already_waited
+
+
+def switched_fork(node):
+    # TODO use notification system when it is ready instead of parsing stderr
+    with open(node.directory / 'stderr.txt') as node_stderr:
+        for line in node_stderr:
+            if 'Switching to fork' in line:
+                return True
+
+    return False
+
+
+def wait_for_irreversible_progress(node, block_num, logging_frequency=5):
+    logger.info(f'Waiting for progress of irreversible block')
+    head_block = get_head_block(node)
+    irreversible_block = get_irreversible_block(node)
+    while irreversible_block < block_num:
+        node.wait_for_block_with_number(head_block+1)
+        head_block = get_head_block(node)
+        irreversible_block = get_irreversible_block(node)
+        if head_block % logging_frequency == 0:
+            logger.info(f"Current head_block_number: {head_block}, irreversible_block_num: {irreversible_block}")
+            logger.info(f"Waiting for irreversible progress until block {block_num}")
+    return irreversible_block, head_block
 
 
 def get_time_offset_from_file(name):
@@ -100,3 +148,67 @@ def create_node_with_database(network, url):
     api_node.config.plugin.append('sql_serializer')
     api_node.config.psql_url = str(url)
     return api_node
+
+
+def get_account_history(session, name, include_reversible=False):
+    acc = session.query(Accounts).filter(Accounts.name == name).one_or_none()
+    if acc == None:
+        acc = session.query(AccountsReversible).filter(AccountsReversible.name == name).one_or_none()
+        if acc == None:
+            return []
+
+    id = acc.id
+    types = []
+
+    acc_ops = session.query(AccountOperations).\
+        filter(AccountOperations.account_id == id).\
+        order_by(AccountOperations.account_op_seq_no).all()
+    for acc_op in acc_ops:
+        operation_id = acc_op.operation_id
+        op = session.query(Operations).filter(Operations.id==operation_id).one()
+        types.append(json.loads(op.body)['type'])
+
+    if include_reversible:
+        acc_ops_reversible = session.query(AccountOperationsReversible).\
+            filter(AccountOperationsReversible.account_id == id).\
+            order_by(AccountOperationsReversible.account_op_seq_no).all()
+        for acc_op in acc_ops_reversible:
+            operation_id = acc_op.operation_id
+            op = session.query(OperationsReversible).filter(OperationsReversible.id==operation_id).one()
+            types.append(json.loads(op.body)['type'])
+
+    return types
+
+
+def import_witnesses_keys(wallet):
+    for name in alpha_witness_names:
+        wallet.api.import_key(Account(name).private_key)
+    for name in beta_witness_names:
+        wallet.api.import_key(Account(name).private_key)
+
+
+def prepare_create_account_trxs(wallet, name):
+    transaction1 = wallet.api.create_account('initminer', name, '', broadcast=False)
+    transaction2 = wallet.api.transfer('initminer', name, Asset.Test(1), 'memo', broadcast=False)
+
+    return [transaction1, transaction2]
+
+
+def prepare_transaction1_multisig(wallet):
+    import_witnesses_keys(wallet)
+    context1 = wallet.in_single_transaction(broadcast=False)
+    with context1:
+        for name in ['witness1-alpha', 'witness1-beta']:
+            wallet.api.transfer(name, "initminer", Asset.Test(1), 'memo')
+    transaction1 = context1.get_response()
+    return transaction1
+
+
+def prepare_transaction2_multisig(wallet):
+    import_witnesses_keys(wallet)
+    context2 = wallet.in_single_transaction(broadcast=False)
+    with context2:
+        for name in ['witness2-alpha', 'witness2-beta']:
+            wallet.api.transfer(name, "initminer", Asset.Test(1), 'memo')
+    transaction2 = context2.get_response()
+    return transaction2
---
+++ tests/integration/system/haf/test_undo_transactions_multisig.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from test_tools import logger, Wallet
+from local_tools import *
+from tables import Transactions, TransactionsReversible, TransactionsMultisig, TransactionsMultisigReversible
+
+
+START_TEST_BLOCK = 111
+
+
+def test_undo_transactions_multisig(world_with_witnesses_and_database):
+    logger.info(f'Start test_undo_transactions_multisig')
+
+    # GIVEN
+    world, session = world_with_witnesses_and_database
+    run_networks(world, Path().resolve())
+
+    node_under_test = world.network('Beta').node('NodeUnderTest')
+    wallet = Wallet(attach_to=node_under_test)
+    trx1 = prepare_transaction1_multisig(wallet)
+    trx2 = prepare_transaction2_multisig(wallet)
+
+    # WHEN
+    make_fork(
+        world,
+        at_block = START_TEST_BLOCK,
+        main_chain_trxs = [trx1],
+        fork_chain_trxs = [trx1, trx2],
+    )
+
+    # THEN
+    trxs_fork = session.query(TransactionsReversible).filter(TransactionsReversible.block_num > START_TEST_BLOCK).all()
+    assert len(trxs_fork) == 2
+    logger.info(f'Found transaction with hash {trxs_fork[0].trx_hash} on block {trxs_fork[0].block_num}, this will be reverted')
+    session.query(TransactionsMultisigReversible).filter(TransactionsMultisigReversible.trx_hash == trxs_fork[0].trx_hash).one()
+    logger.info(f'Found transaction with hash {trxs_fork[1].trx_hash} on block {trxs_fork[1].block_num}, this will be reverted')
+    session.query(TransactionsMultisigReversible).filter(TransactionsMultisigReversible.trx_hash == trxs_fork[1].trx_hash).one()
+
+    after_fork_block = back_from_fork(world)
+    wait_for_irreversible_progress(node_under_test, after_fork_block)
+    trx_main = session.query(Transactions).filter(Transactions.block_num > START_TEST_BLOCK).one()
+    trx_hash_main = trx_main.trx_hash
+    logger.info(f'Found transaction with hash {trx_hash_main} on block {trx_main.block_num}')
+
+    session.query(TransactionsMultisig).filter(TransactionsMultisig.trx_hash == trx_hash_main).one()
