8769320c895aeba2c45be16472e222f20481f08c
mkrzeminski <mkrzeminski@syncad.com>
2022-11-14 13:12:10 +0100

    fIX disable_indexes_of_irreversible AND disable_fk_of_irreversible in api

--- src/hive_fork_manager/hived_api.sql
+++ src/hive_fork_manager/hived_api.sql
@@ -157,7 +157,7 @@ CREATE OR REPLACE FUNCTION hive.disable_indexes_of_irreversible()
 AS
 $BODY$
 BEGIN
-    PERFORM hive.save_and_drop_indexes_constraints( 'hive', 'irreversible_data' );
+    PERFORM hive.save_and_drop_indexes_constraints( 'hive', 'irreversible_data_the_table' );
     PERFORM hive.save_and_drop_indexes_constraints( 'hive', 'blocks' );
     PERFORM hive.save_and_drop_indexes_constraints( 'hive', 'transactions' );
     PERFORM hive.save_and_drop_indexes_constraints( 'hive', 'transactions_multisig' );
@@ -175,7 +175,7 @@ CREATE OR REPLACE FUNCTION hive.disable_fk_of_irreversible()
 AS
 $BODY$
 BEGIN
-    PERFORM hive.save_and_drop_indexes_foreign_keys( 'hive', 'irreversible_data' );
+    PERFORM hive.save_and_drop_indexes_foreign_keys( 'hive', 'irreversible_data_the_table' );
     PERFORM hive.save_and_drop_indexes_foreign_keys( 'hive', 'blocks' );
     PERFORM hive.save_and_drop_indexes_foreign_keys( 'hive', 'transactions' );
     PERFORM hive.save_and_drop_indexes_foreign_keys( 'hive', 'transactions_multisig' );
