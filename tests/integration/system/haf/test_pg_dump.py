3a336c05b17002a28a32b5a25ba6723009e88f5d
mkrzeminski <mkrzeminski@syncad.com>
2022-12-05 13:42:45 +0100

    Introduce tmp_path https://gitlab.syncad.com/hive/haf/-/merge_requests/194#note_113036

--- tests/integration/system/haf/test_pg_dump.py
+++ tests/integration/system/haf/test_pg_dump.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import subprocess
+from pathlib import Path
 from typing import TYPE_CHECKING, Final
 
 import pytest
@@ -20,53 +21,53 @@ DUMP_FILENAME: Final[str] = "adump.Fcsql"
 RESTORE_FROM_TOC = True
 
 
-def pg_restore_from_TOC(target_db_name: str) -> None:
+def pg_restore_from_TOC(target_db_name: str, tmp_path : Path) -> None:
     """ For debugging purposes it is sometimes valuable to display dump contents like this:
     pg_restore --section=pre-data  --disable-triggers  -Fc -f adump-pre-data.sql  adump.Fcsql
     """
     db_name = target_db_name.database
-    original_toc = f'{db_name}_org_.toc'
-    stripped_toc = f'{db_name}_stripped.toc'
+    original_toc = tmp_path / f'{db_name}_org_.toc'
+    stripped_toc = tmp_path / f'{db_name}_stripped.toc'
 
-    shell(f"pg_restore --exit-on-error -l {DUMP_FILENAME} > {original_toc}")
+    shell(f"pg_restore --exit-on-error -l {tmp_path / DUMP_FILENAME} > {original_toc}")
 
     shell(fr"grep -v '[0-9]\+; [0-9]\+ [0-9]\+ SCHEMA - hive'  {original_toc} | grep -v '[0-9]\+; [0-9]\+ [0-9]\+ POLICY hive' > {stripped_toc} ")
     
-    shell(f"pg_restore --exit-on-error --single-transaction  -L {stripped_toc} -d {target_db_name} {DUMP_FILENAME}")
+    shell(f"pg_restore --exit-on-error --single-transaction  -L {stripped_toc} -d {target_db_name} {tmp_path / DUMP_FILENAME}")
 
 
-def pg_restore_from_dump_file_only(target_db_name: str) -> None:
+def pg_restore_from_dump_file_only(target_db_name: str, tmp_path : Path) -> None:
     # restore pre-data
-    shell(f"pg_restore --section=pre-data -Fc -d {target_db_name}   {DUMP_FILENAME}")
+    shell(f"pg_restore --section=pre-data -Fc -d {target_db_name}   {tmp_path / DUMP_FILENAME}")
 
     #restore data
-    shell(f"pg_restore --section=data -Fc --disable-triggers -d {target_db_name}   {DUMP_FILENAME}")
+    shell(f"pg_restore --section=data -Fc --disable-triggers -d {target_db_name}   {tmp_path / DUMP_FILENAME}")
 
     #restore post-data is not needed by far 
 
 
 @pytest.mark.parametrize("pg_restore",[pg_restore_from_TOC, pg_restore_from_dump_file_only])
-def test_pg_dump(database, pg_restore):
+def test_pg_dump(database, pg_restore, tmp_path : Path):
     tt.logger.info(f'Start test_pg_dump')
 
     tt.logger.info(f'Start dump test with {pg_restore.__name__}')
 
     # GIVEN
     source_session, source_db_url = prepare_source_db(database)
-    pg_dump(source_db_url)
+    pg_dump(source_db_url, tmp_path)
 
     target_session, _ = database('postgresql:///dump_target')
     target_db_url = target_session.bind.url
 
     # WHEN
 
-    pg_restore(target_db_url)
+    pg_restore(target_db_url, tmp_path)
 
     # THEN 
 
     compare_databases(source_session,  target_session)
 
-    compare_psql_tool_dumped_schemas(source_db_url.database ,  target_db_url.database)
+    compare_psql_tool_dumped_schemas(source_db_url.database ,  target_db_url.database, tmp_path)
 
     
 def prepare_source_db(database) -> tuple[Session, URL]:
@@ -79,8 +80,8 @@ def prepare_source_db(database) -> tuple[Session, URL]:
     return source_session, source_db_name
 
 
-def pg_dump(db_name : str) -> None:
-    shell(f'pg_dump -Fc -d {db_name} -f {DUMP_FILENAME}')
+def pg_dump(db_name : str, tmp_path : Path) -> None:
+    shell(f'pg_dump -Fc -d {db_name} -f {tmp_path / DUMP_FILENAME}')
 
 def compare_databases(source_session: Session, target_session: Session) -> None:
     ask_for_tables_and_views_sql = f"SELECT table_name FROM information_schema.tables WHERE table_schema = 'hive' ORDER BY table_name"
@@ -101,16 +102,16 @@ def take_table_contents(session: Session, table: str) -> list[Row]:
     return query_all(session, f"SELECT * FROM hive.{table} ORDER BY {columns}")
 
 
-def compare_psql_tool_dumped_schemas(source_db_name: str, target_db_name: str) -> None:
-    source_schema = create_psql_tool_dumped_schema(source_db_name)
-    target_schema = create_psql_tool_dumped_schema(target_db_name)
+def compare_psql_tool_dumped_schemas(source_db_name: str, target_db_name: str, tmp_path : Path) -> None:
+    source_schema = create_psql_tool_dumped_schema(source_db_name, tmp_path)
+    target_schema = create_psql_tool_dumped_schema(target_db_name, tmp_path)
 
     assert source_schema == target_schema
 
     
-def create_psql_tool_dumped_schema(db_name: str) -> str:
+def create_psql_tool_dumped_schema(db_name: str, tmp_path : Path) -> str:
     
-    schema_filename = db_name + '_schema.txt'
+    schema_filename = tmp_path / (db_name + '_schema.txt')
 
     shell(rf"psql -d {db_name} -c '\dn'  > {schema_filename}")
     shell(rf"psql -d {db_name} -c '\d hive.*' >> {schema_filename}")
