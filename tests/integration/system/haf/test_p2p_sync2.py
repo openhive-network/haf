a74c22cf3903e3e054433691a80d80f79e7bca06
Radek Masłowski <rmaslowski@syncad.com>
2023-01-23 09:24:53 +0100

    Add simply p2p test

---
+++ tests/integration/system/haf/test_p2p_sync2.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import test_tools as tt
+
+from haf_local_tools import connect_nodes, get_operations, get_operations_from_database, \
+    prepare_network_with_init_node_and_api_node, set_time_to_offset, verify_operation_in_haf_database
+from haf_local_tools.tables import Operations
+
+
+def test_p2p_sync(database):
+    session = database('postgresql:///haf_block_log')
+
+    api_node, init_node = prepare_network_with_init_node_and_api_node(session, '@2016-01-02 00:00:00')
+    api_node.config.psql_index_threshold = 28810
+
+    init_node.wait_for_block_with_number(5)
+    wallet = tt.Wallet(attach_to=init_node)
+    transaction = wallet.api.create_account('initminer', 'alice', '{}')
+    init_node.wait_for_irreversible_block()
+
+    connect_nodes(init_node, api_node)
+    api_node.run(time_offset=set_time_to_offset(init_node, 10), wait_for_live=True)
+
+    #wait for synchronize api node with haf
+    init_node.wait_number_of_blocks(3)
+
+    verify_operation_in_haf_database('account_create_operation', [transaction], session, Operations)
+
+    operations_in_database = get_operations_from_database(session, Operations, transaction['block_num'])
+    operations = get_operations(init_node, last_block=transaction['block_num'])
+    assert len(operations_in_database) == len(operations)
+
+
+def test_p2p_sync_with_massive_sync(database):
+    session = database('postgresql:///haf_block_log')
+
+    api_node, init_node = prepare_network_with_init_node_and_api_node(session, '@2016-01-02 00:00:00')
+    api_node.config.psql_index_threshold = 28790
+
+    init_node.wait_for_block_with_number(5)
+    wallet = tt.Wallet(attach_to=init_node)
+    transaction = wallet.api.create_account('initminer', 'alice', '{}')
+    init_node.wait_for_irreversible_block()
+
+    connect_nodes(init_node, api_node)
+    api_node.run(time_offset=set_time_to_offset(init_node, 10), wait_for_live=True)
+
+    #wait for synchronize api node with haf
+    init_node.wait_number_of_blocks(3)
+
+    verify_operation_in_haf_database('account_create_operation', [transaction], session, Operations)
+
+    operations_in_database = get_operations_from_database(session, Operations, transaction['block_num'])
+    operations = get_operations(init_node, last_block=transaction['block_num'])
+    assert len(operations_in_database) == len(operations)
