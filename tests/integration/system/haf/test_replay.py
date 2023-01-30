8a8658548eef4e1270dbbab65e9d025f0840d207
Radek Masłowski <rmaslowski@syncad.com>
2023-01-30 13:31:12 +0100

    Add test of replay with disabled or enabled indexes

---
+++ tests/integration/system/haf/test_replay.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import test_tools as tt
+
+from haf_local_tools import connect_nodes, get_operations, get_operations_from_database,\
+    prepare_network_with_init_node_and_api_node, prepare_operations, verify_operation_in_haf_database
+from haf_local_tools.tables import Operations
+
+
+def test_replay_without_disabled_indexes(database):
+    session = database('postgresql:///haf_block_log')
+
+    api_node, init_node = prepare_network_with_init_node_and_api_node(session)
+    api_node.config.psql_index_threshold = 100
+
+    wallet = tt.Wallet(attach_to=init_node)
+
+    transaction_0, transaction_1 = prepare_operations(init_node, wallet)
+
+    init_node.close()
+
+    init_node.run()
+    connect_nodes(init_node, api_node)
+
+    api_node.run(replay_from=init_node.block_log, stop_at_block=20, wait_for_live=False)
+
+    #wait for synchronize api node with haf
+    init_node.wait_number_of_blocks(3)
+
+    verify_operation_in_haf_database('account_create_operation', [transaction_0, transaction_1], session, Operations)
+
+    operations_in_database = get_operations_from_database(session, Operations, transaction_1['block_num'])
+    operations = get_operations(init_node, last_block=transaction_1['block_num'])
+    assert len(operations_in_database) == len(operations)
+
+
+def test_replay_with_disabled_indexes(database):
+    session = database('postgresql:///haf_block_log')
+
+    api_node, init_node = prepare_network_with_init_node_and_api_node(session)
+    api_node.config.psql_index_threshold = 10
+
+    wallet = tt.Wallet(attach_to=init_node)
+
+    transaction_0, transaction_1 = prepare_operations(init_node, wallet)
+
+    init_node.close()
+
+    init_node.run()
+    connect_nodes(init_node, api_node)
+
+    api_node.run(replay_from=init_node.block_log, stop_at_block=20, wait_for_live=False)
+
+    #wait for synchronize api node with haf
+    init_node.wait_number_of_blocks(3)
+
+    verify_operation_in_haf_database('account_create_operation', [transaction_0, transaction_1], session, Operations)
+
+    operations_in_database = get_operations_from_database(session, Operations, transaction_1['block_num'])
+    operations = get_operations(init_node, last_block=transaction_1['block_num'])
+    assert len(operations_in_database) == len(operations)
