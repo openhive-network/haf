5e70c4c0dd2874701272b70ef884d0a18d543eed
Radek Masłowski <rmaslowski@syncad.com>
2023-02-13 13:38:35 +0100

    fixup! Add simply p2p test

--- tests/integration/system/haf/test_p2p_sync2.py
+++ tests/integration/system/haf/test_p2p_sync2.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import test_tools as tt
 
 from haf_local_tools import connect_nodes, get_operations, get_operations_from_database, \
-    prepare_network_with_init_node_and_api_node, set_time_to_offset, verify_operation_in_haf_database
+    prepare_network_with_init_node_and_api_node, prepare_and_send_transactions,\
+    set_time_to_offset, verify_operation_in_haf_database
 from haf_local_tools.tables import Operations
 
 
@@ -13,10 +14,7 @@ def test_p2p_sync(database):
     api_node, init_node = prepare_network_with_init_node_and_api_node(session)
     api_node.config.psql_index_threshold = 2147483647
 
-    init_node.wait_for_block_with_number(5)
-    wallet = tt.Wallet(attach_to=init_node)
-    transaction = wallet.api.create_account('initminer', 'alice', '{}')
-    init_node.wait_for_irreversible_block()
+    transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
 
     connect_nodes(init_node, api_node)
     api_node.run(wait_for_live=True)
@@ -25,10 +23,10 @@ def test_p2p_sync(database):
     #wait for synchronize api node with haf
     init_node.wait_number_of_blocks(3)
 
-    verify_operation_in_haf_database('account_create_operation', [transaction], session, Operations)
+    verify_operation_in_haf_database('account_create_operation', [transaction_0, transaction_1], session, Operations)
 
-    operations_in_database = get_operations_from_database(session, Operations, transaction['block_num'])
-    operations = get_operations(init_node, last_block=transaction['block_num'])
+    operations_in_database = get_operations_from_database(session, Operations, transaction_1['block_num'])
+    operations = get_operations(init_node, last_block=transaction_1['block_num'])
     assert len(operations_in_database) == len(operations)
 
 
@@ -37,10 +35,7 @@ def test_p2p_sync_with_massive_sync(database):
 
     api_node, init_node = prepare_network_with_init_node_and_api_node(session)
 
-    init_node.wait_for_block_with_number(5)
-    wallet = tt.Wallet(attach_to=init_node)
-    transaction = wallet.api.create_account('initminer', 'alice', '{}')
-    init_node.wait_for_irreversible_block()
+    transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
 
     connect_nodes(init_node, api_node)
     api_node.run(wait_for_live=True)
@@ -49,8 +44,8 @@ def test_p2p_sync_with_massive_sync(database):
     #wait for synchronize api node with haf
     init_node.wait_number_of_blocks(3)
 
-    verify_operation_in_haf_database('account_create_operation', [transaction], session, Operations)
+    verify_operation_in_haf_database('account_create_operation', [transaction_0, transaction_1], session, Operations)
 
-    operations_in_database = get_operations_from_database(session, Operations, transaction['block_num'])
-    operations = get_operations(init_node, last_block=transaction['block_num'])
+    operations_in_database = get_operations_from_database(session, Operations, transaction_1['block_num'])
+    operations = get_operations(init_node, last_block=transaction_1['block_num'])
     assert len(operations_in_database) == len(operations)
