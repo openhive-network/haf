98961841592bca70706f9f04d45b5311f92e0872
Radek Masłowski <rmaslowski@syncad.com>
2023-02-13 12:57:01 +0100

    fixup! Add test of replay with disabled or enabled indexes

--- tests/integration/system/haf/test_replay.py
+++ tests/integration/system/haf/test_replay.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import test_tools as tt
 
 from haf_local_tools import connect_nodes, get_operations, get_operations_from_database,\
-    prepare_network_with_init_node_and_api_node, prepare_operations, verify_operation_in_haf_database
+    prepare_network_with_init_node_and_api_node, prepare_and_send_transactions, verify_operation_in_haf_database
 from haf_local_tools.tables import Operations
 
 
@@ -15,7 +15,7 @@ def test_replay_without_disabled_indexes(database):
 
     wallet = tt.Wallet(attach_to=init_node)
 
-    transaction_0, transaction_1 = prepare_operations(init_node, wallet)
+    transaction_0, transaction_1 = prepare_and_send_transactions(init_node, wallet)
 
     init_node.close()
 
@@ -43,7 +43,7 @@ def test_replay_with_disabled_indexes(database):
 
     wallet = tt.Wallet(attach_to=init_node)
 
-    transaction_0, transaction_1 = prepare_operations(init_node, wallet)
+    transaction_0, transaction_1 = prepare_and_send_transactions(init_node, wallet)
 
     init_node.close()
 
