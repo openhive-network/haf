bfea49c96c2cad4ccbac5cf4f19e21e652852743
Marcin Ickiewicz <mickiewicz@syncad.com>
2023-02-17 13:43:24 +0100

    test to proof incorrectness of refresh_irreversible_block_for_all_contexts

---
+++ tests/integration/functional/hive_fork_manager/hived_api/set_irreversible_and_context_blocks_view_test.sql
@@ -0,0 +1,112 @@
+DROP FUNCTION IF EXISTS test_given;
+CREATE FUNCTION test_given()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+BEGIN
+    PERFORM hive.app_create_context( 'context' );
+    CREATE TABLE table1( id INT ) INHERITS( hive.context );
+    PERFORM hive.app_next_block( 'context' );
+
+    INSERT INTO hive.fork( id, block_num, time_of_fork)
+    VALUES ( 2, 6, '2020-06-22 19:10:25-07'::timestamp ),
+           ( 3, 7, '2020-06-22 19:10:25-07'::timestamp );
+
+    INSERT INTO hive.events_queue
+    VALUES
+           ( 1, 'NEW_BLOCK', 4 )
+         , ( 2, 'NEW_BLOCK', 5 )
+         , ( 3, 'NEW_BLOCK', 6 )
+         , ( 4, 'NEW_BLOCK', 7 )
+         , ( 5, 'NEW_BLOCK', 8 )
+         , ( 6, 'NEW_BLOCK', 9 )
+         , ( 7, 'BACK_FROM_FORK', 2 )
+         , ( 8, 'NEW_BLOCK', 7 )
+         , ( 9, 'NEW_BLOCK', 8 )
+         , ( 10, 'NEW_IRREVERSIBLE', 4 )
+         , ( 11, 'NEW_BLOCK', 9 ) -- context on this event
+         , ( 12, 'BACK_FROM_FORK', 3)
+         , ( 13, 'NEW_BLOCK', 8)
+         , ( 14, 'NEW_BLOCK', 9)
+         , ( 15, 'NEW_BLOCK', 10)
+         , ( 16, 'NEW_IRREVERSIBLE', 5 )
+         , ( 17, 'NEW_IRREVERSIBLE', 6 )
+         , ( 18, 'NEW_IRREVERSIBLE', 7 )
+    ;
+
+    INSERT INTO hive.blocks
+    VALUES
+          ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+        , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:22-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+        , ( 3, '\xBADD30', '\xCAFE30', '2016-06-22 19:10:23-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+        , ( 4, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+        , ( 5, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+    ;
+
+    INSERT INTO hive.accounts( id, name, block_num )
+    VALUES (5, 'initminer', 1)
+         , (6, 'alice', 1)
+         , (7, 'bob', 1)
+    ;
+
+    INSERT INTO hive.blocks_reversible
+    VALUES
+          ( 4, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 1 )
+        , ( 5, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:55-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 1 )
+        , ( 6, '\xBADD60', '\xCAFE60', '2016-06-22 19:10:26-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 1 )
+        , ( 7, '\xBADD7001', '\xCAFE70', '2016-06-22 19:10:27-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 1 ) -- must be overriden by fork 2
+        , ( 8, '\xBADD8001', '\xCAFE80', '2016-06-22 19:10:28-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 1 ) -- must be overriden by fork 2
+        , ( 9, '\xBADD9001', '\xCAFE90', '2016-06-22 19:10:29-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 1 ) -- must be overriden by fork 2
+        , ( 7, '\xBADD70', '\xCAFE70', '2016-06-22 19:10:27-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 2 )
+        , ( 8, '\xBADD8002', '\xCAFE80', '2016-06-22 19:10:28-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 2 )
+        , ( 9, '\xBADD9002', '\xCAFE90', '2016-06-22 19:10:29-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 2 )
+        , ( 8, '\xBADD8003', '\xCAFE80', '2016-06-22 19:10:30-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 3 )
+        , ( 9, '\xBADD90', '\xCAFE90', '2016-06-22 19:10:31-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 3 )
+        , ( 10, '\xBADD1A', '\xCAFE1A', '2016-06-22 19:10:32-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000, 3 )
+    ;
+
+    UPDATE hive.contexts SET fork_id = 2, irreversible_block = 4, current_block_num = 9, events_id = 11;
+END;
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_when;
+CREATE FUNCTION test_when()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+BEGIN
+    --Add event NI(8) (after fork 3)
+    PERFORM hive.set_irreversible( 8 );
+END
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_then;
+CREATE FUNCTION test_then()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+STABLE
+AS
+$BODY$
+BEGIN
+    -- bot block 8 and 9 must be in fork 2 version, sine application may still process block 9 from fork 2
+    ASSERT ( SELECT COUNT(*) FROM hive.context_blocks_view WHERE num = 8 OR num = 9 ) = 2, 'No expected block nr. 8 in the hive.context_blocks_view';
+    ASSERT NOT EXISTS (
+        SELECT * FROM hive.context_blocks_view WHERE num = 8 OR num = 9
+        EXCEPT SELECT * FROM ( VALUES
+                  (8, '\xBADD8002'::bytea, '\xCAFE80'::bytea, '2016-06-22 19:10:28-07'::timestamp, 5, '\x4007'::bytea, '[]'::jsonb, '\x2157'::bytea, 'STM65w')
+                , (9, '\xBADD9002'::bytea, '\xCAFE90'::bytea, '2016-06-22 19:10:29-07'::timestamp, 5, '\x4007'::bytea, '[]'::jsonb, '\x2157'::bytea, 'STM65w')
+        ) as pattern
+    ) , 'Unexpected rows in the hive.context_blocks_view';
+END
+$BODY$
+;
+
+
+
+
