7c1a4cf93685f7f47080d4a2d88bc81265efdfc2
Marcin Sobczyk <msobczyk@syncad.com>
2023-03-02 13:35:37 +0100

    Use pytest parametrize

--- tests/integration/system/haf/test_p2p_sync.py
+++ tests/integration/system/haf/test_p2p_sync.py
@@ -1,34 +1,16 @@
+import pytest
+
 from haf_local_tools import connect_nodes, get_operations, get_operations_from_database, \
     prepare_network_with_init_node_and_api_node, prepare_and_send_transactions, verify_operation_in_haf_database
 from haf_local_tools.tables import Operations
 
 
-def test_p2p_sync(database):
+@pytest.mark.parametrize("psql_index_threshold", [2147483647, 100000])
+def test_p2p_sync(database, psql_index_threshold):
     session = database('postgresql:///haf_block_log')
 
     api_node, init_node = prepare_network_with_init_node_and_api_node(session)
-    api_node.config.psql_index_threshold = 2147483647
-
-    transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
-
-    connect_nodes(init_node, api_node)
-    api_node.run(wait_for_live=True)
-
-    # wait for synchronize api node with haf
-    head = init_node.get_last_block_number()
-    api_node.wait_for_block_with_number(head)
-
-    verify_operation_in_haf_database('account_create_operation', [transaction_0, transaction_1], session, Operations)
-
-    operations_in_database = get_operations_from_database(session, Operations, transaction_1['block_num'])
-    operations = get_operations(init_node, last_block=transaction_1['block_num'])
-    assert len(operations_in_database) == len(operations)
-
-
-def test_p2p_sync_with_massive_sync(database):
-    session = database('postgresql:///haf_block_log')
-
-    api_node, init_node = prepare_network_with_init_node_and_api_node(session)
+    api_node.config.psql_index_threshold = psql_index_threshold
 
     transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
 
--- tests/integration/system/haf/test_replay.py
+++ tests/integration/system/haf/test_replay.py
@@ -1,30 +1,16 @@
+import pytest
+
 from haf_local_tools import get_operations, get_operations_from_database,prepare_network_with_init_node_and_api_node,\
     prepare_and_send_transactions, verify_operation_in_haf_database
 from haf_local_tools.tables import Operations
 
 
-def test_replay_without_disabled_indexes(database):
+@pytest.mark.parametrize("psql_index_threshold", [100, 10])
+def test_replay_without_disabled_indexes(database, psql_index_threshold):
     session = database('postgresql:///haf_block_log')
 
     api_node, init_node = prepare_network_with_init_node_and_api_node(session)
-    api_node.config.psql_index_threshold = 100
-
-    transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
-
-    api_node.run(replay_from=init_node.block_log, stop_at_block=20, wait_for_live=False)
-
-    verify_operation_in_haf_database('account_create_operation', [transaction_0, transaction_1], session, Operations)
-
-    operations_in_database = get_operations_from_database(session, Operations, transaction_1['block_num'])
-    operations = get_operations(init_node, last_block=transaction_1['block_num'])
-    assert len(operations_in_database) == len(operations)
-
-
-def test_replay_with_disabled_indexes(database):
-    session = database('postgresql:///haf_block_log')
-
-    api_node, init_node = prepare_network_with_init_node_and_api_node(session)
-    api_node.config.psql_index_threshold = 10
+    api_node.config.psql_index_threshold = psql_index_threshold
 
     transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
 
