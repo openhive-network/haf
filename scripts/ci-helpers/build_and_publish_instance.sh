af88b2583016878c37312d3b57e2d36e66f2cbcb
Konrad Botor <kbotor@syncad.com>
2023-03-06 13:53:42 +0100

    Replaced scripts/ci-helpers/build_and_publish_instance.sh with a symbolic link - ref. hive#434

--- scripts/ci-helpers/build_and_publish_instance.sh
+++ scripts/ci-helpers/build_and_publish_instance.sh
@@ -1,7 +1,1 @@
-#! /bin/bash
-
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-SRC_DIR="$SCRIPT_DIR/../.."
-
-export SRC_DIR_OFFSET="../../.."
-"$SRC_DIR/hive/scripts/ci-helpers/build_and_publish_instance.sh" "$@"
+../../hive/scripts/ci-helpers/build_and_publish_instance.sh
