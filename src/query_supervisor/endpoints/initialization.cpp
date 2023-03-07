a31928295c59408e427419cfdf53033026dd48c4
Marcin Ickiewicz <mickiewicz@syncad.com>
2023-03-07 11:20:30 +0100

    refactor handler: instead of add thread to backend use pg timeouts

--- src/query_supervisor/endpoints/initialization.cpp
+++ src/query_supervisor/endpoints/initialization.cpp
@@ -2,16 +2,11 @@
 
 #include "include/psql_utils/query_handler/timeout_query_handler.h"
 
-#include "include/psql_utils/custom_configuration.h"
-
-PsqlTools::PsqlUtils::CustomConfiguration g_customConfiguration( "querysupervisor" );
-
 extern "C" {
   PG_MODULE_MAGIC;
 
 void _PG_init(void) {
   PsqlTools::PsqlUtils::QueryHandler::initialize<PsqlTools::PsqlUtils::TimeoutQueryHandler>();
-  g_customConfiguration.addStringOption( "hafadmin", "Name of haf admin role", "Name of haf admin role", "haf_admin" );
 }
 
 void _PG_fini(void) {
