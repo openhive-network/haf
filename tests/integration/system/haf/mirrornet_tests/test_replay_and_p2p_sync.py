bdb12b99afaaf0700fc57bc5ebdc9fbbc4877323
Radek Masłowski <rmaslowski@syncad.com>
2023-03-15 15:05:26 +0000

    wip

--- tests/integration/system/haf/mirrornet_tests/test_replay_and_p2p_sync.py
+++ tests/integration/system/haf/mirrornet_tests/test_replay_and_p2p_sync.py
@@ -1,3 +1,5 @@
+import time
+
 import test_tools as tt
 import datetime
 import json
@@ -7,16 +9,20 @@ from haf_local_tools import connect_nodes, get_operations, get_operations_from_d
 from haf_local_tools.tables import Operations
 
 def test_replay_and_p2p_sync(database):
-    # session = database('postgresql://dev@172.17.0.1:5432/haf_block_log')
+    session = database('postgresql://dev@172.17.0.1:5432/haf_block_log')
+    timestamp_origin = '@2016-09-15 19:47:24'
+    timestamp_blok_100k = '@2016-03-28 04:07:21'
 
     max_years = 292  # Ref: https://github.com/python/cpython/blob/889b0b9bf95651fc05ad532cc4a66c0f8ff29fc2/Include/cpython/pytime.h
     sleep_time = datetime.timedelta(days=max_years * 365).total_seconds()
-    witnesses = ["rabbit-70", "kushed", "delegate.lafona", "wackou", "complexring", "jesta", "xeldal", "riverhead", "clayop", "steemed", "smooth.witness", "ihashfury", "joseph", "datasecuritynode", "boatymcboatface", "steemychicken1", "roadscape", "pharesim", "abit", "blocktrades", "arhag", "bitcube", "witness.svk", "gxt-1080-sc-0003", "steve-walschot", "bhuz", "liondani", "rabbit-63", "pfunk"]
-    # witnesses = ["steemit18", "sminer4", "hanyuu", "debruyne", "steemit16", "sminer36", "steemit", "sminer50", "mottler-6", "sminer38", "binaryfate", "sminer48", "sminer37", "sminer5", "steemit17", "steemit1", "sminer39", "moderator", "sminer49"]
+    witnesses_5m = ["rabbit-70", "kushed", "delegate.lafona", "wackou", "complexring", "jesta", "xeldal", "riverhead", "clayop", "steemed", "smooth.witness", "ihashfury", "joseph", "datasecuritynode", "boatymcboatface", "steemychicken1", "roadscape", "pharesim", "abit", "blocktrades", "arhag", "bitcube", "witness.svk", "gxt-1080-sc-0003", "steve-walschot", "bhuz", "liondani", "rabbit-63", "pfunk"]
+    witnesses_100k = ["steemit18", "sminer4", "hanyuu", "debruyne", "steemit16", "sminer36", "steemit", "sminer50", "mottler-6", "sminer38", "binaryfate", "sminer48", "sminer37", "sminer5", "steemit17", "steemit1", "sminer39", "moderator", "sminer49"]
     raw_node = tt.RawNode()
     path_block_log_5m = '/workspace/haf/tests/integration/system/haf/mirrornet_tests/block_log_5m/block_log'
     path_block_log_3m = '/workspace/haf/tests/integration/system/haf/mirrornet_tests/block_log_3m/block_log'
-    raw_node.config.witness = witnesses
+    path_block_log_50k = '/workspace/haf/tests/integration/system/haf/mirrornet_tests/block_log_50k/block_log'
+    path_block_log_100k = '/workspace/haf/tests/integration/system/haf/mirrornet_tests/block_log_100k/block_log'
+    raw_node.config.witness = witnesses_100k
     raw_node.config.private_key = '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n'
     raw_node.config.shared_file_size = '2G'
     raw_node.config.enable_stale_production = True
@@ -25,7 +31,7 @@ def test_replay_and_p2p_sync(database):
     raw_node.config.plugin = 'condenser_api database_api block_api market_history_api network_broadcast_api witness account_by_key account_by_key_api wallet_bridge_api'
     # raw_node.config.plugin = 'condenser_api block_api'
 
-    raw_node.run(replay_from=path_block_log_5m, time_offset='@2016-09-15 19:47:24', wait_for_live=True, timeout=sleep_time, arguments=['--chain-id', '42', '--skeleton-key', '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n'])
+    raw_node.run(replay_from=path_block_log_100k, time_offset=timestamp_blok_100k, wait_for_live=True, timeout=sleep_time, arguments=['--chain-id', '42', '--skeleton-key', '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n'])
 
     head_block_num = raw_node.api.condenser.get_dynamic_global_properties()['head_block_number']
     head_block_timestamp = raw_node.api.block.get_block(block_num=head_block_num)['block']['timestamp']
@@ -35,11 +41,12 @@ def test_replay_and_p2p_sync(database):
     api_node = tt.RawNode()
     connect_nodes(raw_node, api_node)
     api_node.config.shared_file_size = '2G'
-    # api_node.config.plugin.append('sql_serializer')
-    # api_node.config.psql_url = str(session.get_bind().url)
+    api_node.config.plugin.append('sql_serializer')
+    api_node.config.psql_url = str(session.get_bind().url)
 
-    api_node.run(replay_from=path_block_log_3m, time_offset = tt.Time.serialize(absolute_start_time, format_=tt.Time.TIME_OFFSET_FORMAT), wait_for_live=True, timeout=sleep_time,  arguments=['--chain-id', '42'])
+    api_node.run(replay_from=path_block_log_50k, time_offset = tt.Time.serialize(absolute_start_time, format_=tt.Time.TIME_OFFSET_FORMAT), wait_for_live=True, timeout=sleep_time,  arguments=['--chain-id', '42'])
 
-    tt.logger.info(f'raw_node last block number:  {raw_node.get_last_block_number()}')
-    tt.logger.info(f'api last block number:  {api_node.get_last_block_number()}')
+    # tt.logger.info(f'raw_node last block number:  {raw_node.get_last_block_number()}')
+    # tt.logger.info(f'api last block number:  {api_node.get_last_block_number()}')
 
+    time.sleep(150)
