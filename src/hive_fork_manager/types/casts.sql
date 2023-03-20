fc051db54621bd6b57b8fc19d810546dd76a4ce5
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-20 12:59:20 +0100

    Cast for comment_options_operation

--- src/hive_fork_manager/types/casts.sql
+++ src/hive_fork_manager/types/casts.sql
@@ -8,3 +8,12 @@ CREATE CAST (hive.operation AS hive.comment_operation)
   WITH FUNCTION hive._operation_to_comment_operation
   AS ASSIGNMENT;
 
+CREATE OR REPLACE FUNCTION hive._operation_to_comment_options_operation(
+  hive.operation
+) RETURNS hive.comment_options_operation LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE
+AS 'MODULE_PATHNAME',
+'operation_to_comment_options_operation';
+
+CREATE CAST (hive.operation AS hive.comment_options_operation)
+  WITH FUNCTION hive._operation_to_comment_options_operation
+  AS ASSIGNMENT;
