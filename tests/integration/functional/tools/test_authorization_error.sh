85fc2cecdded7b002bead7a6c1fa23831c7a45eb
Marcin Ickiewicz <mickiewicz@syncad.com>
2023-03-22 11:27:29 +0100

    testing alice and bob

--- tests/integration/functional/tools/test_authorization_error.sh
+++ tests/integration/functional/tools/test_authorization_error.sh
@@ -4,10 +4,11 @@ extension_path=$1
 test_path=$2;
 setup_scripts_dir_path=$3;
 postgres_port=$4;
+preload_libraries=$5;
 
 . ./tools/common.sh
 
-setup_test_database "$setup_scripts_dir_path" "$postgres_port" "$test_path"
+setup_test_database "$setup_scripts_dir_path" "$postgres_port" "$test_path" "$preload_libraries"
 
 trap on_exit EXIT;
 
@@ -20,24 +21,24 @@ psql -p $postgres_port -d $DB_NAME -a -v ON_ERROR_STOP=on -f  ${test_path};
 users="alice bob"
 tests="given when error"
 
-# you can use alice_test_given, alice_tes_when, alice_test_error and their bob's equivalents
+# you can use alice_test_given, alice_test_when, alice_test_error and their bob's equivalents
 
 for user in ${users}; do
   for testfun in ${tests}; do
-    sql_code="DO \$$
+    sql_code="DO \$\$
     BEGIN
       BEGIN
         PERFORM '${user}_test_${testfun}()'::regprocedure;
       EXCEPTION WHEN undefined_function THEN
       END;
-    END \$$;"
+    END \$\$;"
 
     psql postgresql://${user}:test@localhost:$postgres_port/$DB_NAME --username=${user} -a -v ON_ERROR_STOP=on -c "${sql_code}";
     result=$?;
 
     # shellcheck disable=SC2170
-    if [ "${testfun}" == "error" ]; then
-      valuate_error_result ${result}
+    if [ "${testfun}" = "error" ]; then
+      evaluate_error_result ${result}
     else
       evaluate_result ${result}
     fi
