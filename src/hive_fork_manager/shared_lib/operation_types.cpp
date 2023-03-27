235a1a96be148e7574d0ca7fd3b393733e5ab766
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-27 13:34:29 +0200

    Implement cast fram operation to account_create_with_delegation_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -410,4 +410,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::account_create_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_account_create_with_delegation_operation );
+  Datum operation_to_account_create_with_delegation_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::account_create_with_delegation_operation>(op);
+  }
 }
