8b1f8a34fe8872b3e9c11c926f8da9163170986b
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-28 16:47:39 +0200

    Implement cast from operation to escrow_transfer_operation

--- src/hive_fork_manager/types/casts.sql
+++ src/hive_fork_manager/types/casts.sql
@@ -257,3 +257,13 @@ AS 'MODULE_PATHNAME',
 CREATE CAST (hive.operation AS hive.escrow_release_operation)
   WITH FUNCTION hive._operation_to_escrow_release_operation
   AS ASSIGNMENT;
+
+CREATE OR REPLACE FUNCTION hive._operation_to_escrow_transfer_operation(
+  hive.operation
+) RETURNS hive.escrow_transfer_operation LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE
+AS 'MODULE_PATHNAME',
+'operation_to_escrow_transfer_operation';
+
+CREATE CAST (hive.operation AS hive.escrow_transfer_operation)
+  WITH FUNCTION hive._operation_to_escrow_transfer_operation
+  AS ASSIGNMENT;
