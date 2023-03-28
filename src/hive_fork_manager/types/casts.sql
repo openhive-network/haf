11fa349ec50e9cb6cf5cf5ebb4d267122181ec78
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-28 10:29:03 +0200

    Implement cast from operation to create_claimed_account_operation

--- src/hive_fork_manager/types/casts.sql
+++ src/hive_fork_manager/types/casts.sql
@@ -157,3 +157,13 @@ AS 'MODULE_PATHNAME',
 CREATE CAST (hive.operation AS hive.convert_operation)
   WITH FUNCTION hive._operation_to_convert_operation
   AS ASSIGNMENT;
+
+CREATE OR REPLACE FUNCTION hive._operation_to_create_claimed_account_operation(
+  hive.operation
+) RETURNS hive.create_claimed_account_operation LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE
+AS 'MODULE_PATHNAME',
+'operation_to_create_claimed_account_operation';
+
+CREATE CAST (hive.operation AS hive.create_claimed_account_operation)
+  WITH FUNCTION hive._operation_to_create_claimed_account_operation
+  AS ASSIGNMENT;
