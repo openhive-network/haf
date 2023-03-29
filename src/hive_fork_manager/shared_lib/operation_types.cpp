986a436d859b4e66be88d34266519d1a977a2469
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-29 10:23:12 +0200

    Implement cast from operation to limit_order_create2_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -641,4 +641,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::limit_order_cancel_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_limit_order_create2_operation );
+  Datum operation_to_limit_order_create2_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::limit_order_create2_operation>(op);
+  }
 }
