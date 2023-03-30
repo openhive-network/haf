b6bfc62825acde6e054a2c00a0feeec3b18429bf
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-30 15:46:36 +0200

    Implement cast from operation to set_withdraw_vesting_route_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -828,4 +828,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::set_reset_account_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_set_withdraw_vesting_route_operation );
+  Datum operation_to_set_withdraw_vesting_route_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::set_withdraw_vesting_route_operation>(op);
+  }
 }
