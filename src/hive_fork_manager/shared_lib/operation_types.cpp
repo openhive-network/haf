1acfdaf3e7013e206b05434ed1a4ab3c75cb3fff
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-30 16:07:41 +0200

    Implement cast from operation to transfer_to_savings_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -849,4 +849,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::transfer_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_transfer_to_savings_operation );
+  Datum operation_to_transfer_to_savings_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::transfer_to_savings_operation>(op);
+  }
 }
