81ee477e2de0d96c6452e15c60129a5e4a53a6a9
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-30 16:21:52 +0200

    Implement cast from operation to transfer_to_vesting_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -856,4 +856,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::transfer_to_savings_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_transfer_to_vesting_operation );
+  Datum operation_to_transfer_to_vesting_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::transfer_to_vesting_operation>(op);
+  }
 }
--- src/hive_fork_manager/types/casts.sql
+++ src/hive_fork_manager/types/casts.sql
@@ -417,3 +417,13 @@ AS 'MODULE_PATHNAME',
 CREATE CAST (hive.operation AS hive.transfer_to_savings_operation)
   WITH FUNCTION hive._operation_to_transfer_to_savings_operation
   AS ASSIGNMENT;
+
+CREATE OR REPLACE FUNCTION hive._operation_to_transfer_to_vesting_operation(
+  hive.operation
+) RETURNS hive.transfer_to_vesting_operation LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE
+AS 'MODULE_PATHNAME',
+'operation_to_transfer_to_vesting_operation';
+
+CREATE CAST (hive.operation AS hive.transfer_to_vesting_operation)
+  WITH FUNCTION hive._operation_to_transfer_to_vesting_operation
+  AS ASSIGNMENT;
