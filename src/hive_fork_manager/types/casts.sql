c9f2ee2725ac5ef8e1a6192768fe94a21c25c598
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 14:59:14 +0200

    Implement cast from operation to fill_recurrent_transfer_operation

--- src/hive_fork_manager/types/casts.sql
+++ src/hive_fork_manager/types/casts.sql
@@ -657,3 +657,13 @@ AS 'MODULE_PATHNAME',
 CREATE CAST (hive.operation AS hive.fill_order_operation)
   WITH FUNCTION hive._operation_to_fill_order_operation
   AS ASSIGNMENT;
+
+CREATE OR REPLACE FUNCTION hive._operation_to_fill_recurrent_transfer_operation(
+  hive.operation
+) RETURNS hive.fill_recurrent_transfer_operation LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE
+AS 'MODULE_PATHNAME',
+'operation_to_fill_recurrent_transfer_operation';
+
+CREATE CAST (hive.operation AS hive.fill_recurrent_transfer_operation)
+  WITH FUNCTION hive._operation_to_fill_recurrent_transfer_operation
+  AS ASSIGNMENT;
