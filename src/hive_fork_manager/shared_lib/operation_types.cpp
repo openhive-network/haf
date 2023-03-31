07d8c03965429c9d0c9130751201c063b9335a44
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 13:59:18 +0200

    Implement cast from operation to clear_null_account_balance_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -991,4 +991,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::changed_recovery_account_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_clear_null_account_balance_operation );
+  Datum operation_to_clear_null_account_balance_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::clear_null_account_balance_operation>(op);
+  }
 }
