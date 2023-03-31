98059f6e93faf274214a802277ee5f42e4fb9fda
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 14:51:34 +0200

    Implement cast from operation to fill_collateralized_convert_request_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -1065,4 +1065,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::failed_recurrent_transfer_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_fill_collateralized_convert_request_operation );
+  Datum operation_to_fill_collateralized_convert_request_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::fill_collateralized_convert_request_operation>(op);
+  }
 }
