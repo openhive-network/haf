262447650bc47a33fabe4428dbbfaa558f93d803
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 15:29:49 +0200

    Implement cast from operation to ineffective_delete_comment_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -1128,4 +1128,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::hardfork_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_ineffective_delete_comment_operation );
+  Datum operation_to_ineffective_delete_comment_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::ineffective_delete_comment_operation>(op);
+  }
 }
