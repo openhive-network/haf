a1a98b2cd5af319e166fd8db7ef944e84886494d
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 15:19:31 +0200

    Implement cast from operation to hardfork_hive_restore_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -1114,4 +1114,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::hardfork_hive_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_hardfork_hive_restore_operation );
+  Datum operation_to_hardfork_hive_restore_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::hardfork_hive_restore_operation>(op);
+  }
 }
