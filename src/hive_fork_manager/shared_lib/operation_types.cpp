e20aba63cd22ac6758e7b5522f14b7d3ec524af1
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 12:46:34 +0200

    Implement cast from operation to account_created_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -970,4 +970,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::update_proposal_votes_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_account_created_operation );
+  Datum operation_to_account_created_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::account_created_operation>(op);
+  }
 }
