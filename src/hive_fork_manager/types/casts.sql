502e2caa040413dbd053445b2d58125895eb5fa8
Krzysztof Leśniak <klesniak@syncad.com>
2023-03-31 10:13:24 +0200

    Implement cast from operation to create_proposal_operation

--- src/hive_fork_manager/types/casts.sql
+++ src/hive_fork_manager/types/casts.sql
@@ -447,3 +447,13 @@ AS 'MODULE_PATHNAME',
 CREATE CAST (hive.operation AS hive.witness_update_operation)
   WITH FUNCTION hive._operation_to_witness_update_operation
   AS ASSIGNMENT;
+
+CREATE OR REPLACE FUNCTION hive._operation_to_create_proposal_operation(
+  hive.operation
+) RETURNS hive.create_proposal_operation LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE
+AS 'MODULE_PATHNAME',
+'operation_to_create_proposal_operation';
+
+CREATE CAST (hive.operation AS hive.create_proposal_operation)
+  WITH FUNCTION hive._operation_to_create_proposal_operation
+  AS ASSIGNMENT;
