5274066222003e4fcbb2de8e0b8a9b4b30bb64bd
Krzysztof Leśniak <klesniak@syncad.com>
2023-04-03 12:48:39 +0200

    Implemented cast from operation to escrow_rejected_operation

--- src/hive_fork_manager/shared_lib/operation_types.cpp
+++ src/hive_fork_manager/shared_lib/operation_types.cpp
@@ -1254,4 +1254,11 @@ extern "C"
     _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
     return operation_to<hive::protocol::escrow_approved_operation>(op);
   }
+
+  PG_FUNCTION_INFO_V1( operation_to_escrow_rejected_operation );
+  Datum operation_to_escrow_rejected_operation( PG_FUNCTION_ARGS )
+  {
+    _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
+    return operation_to<hive::protocol::escrow_rejected_operation>(op);
+  }
 }
