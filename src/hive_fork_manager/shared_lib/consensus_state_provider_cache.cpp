38cb5c3d8dd5dfcc5abae5bcf86043498866bbff
mkrzeminski <mkrzeminski@syncad.com>
2023-04-19 23:31:50 +0000

    Cleaning 1

---
+++ src/hive_fork_manager/shared_lib/consensus_state_provider_cache.cpp
@@ -0,0 +1,4 @@
+#include "consensus_state_provider_cache.hpp"
+#include <string>
+#include <unordered_map>
+
---
+++ src/hive_fork_manager/shared_lib/consensus_state_provider_cache.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+// #include "hive/chain/database.hpp"
+// namespace consensus_state_provider
+// {
+//     class cache
+//     {
+//     public:
+//         bool has_context(const char* context);
+//         void add(const char* context, hive::chain::database& a_db);
+
+//     };
+// }
