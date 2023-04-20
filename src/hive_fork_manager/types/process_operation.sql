a61decc2b806a9e38ef29451eb7246fb847a21f8
Krzysztof Leśniak <klesniak@syncad.com>
2023-04-20 13:13:24 +0200

    Use to_regprocedure to check function existence
    This reduces query time
    
    `explain analyze select process_operation(body, 'nonexistent') from operations limit 100000;`
    
    from ~640ms to ~510ms.

--- src/hive_fork_manager/types/process_operation.sql
+++ src/hive_fork_manager/types/process_operation.sql
@@ -12,17 +12,10 @@ BEGIN
   -- Find the name of actual operation type
   SELECT hive.operation_type_name(operation) INTO STRICT __operation_type;
 
-  BEGIN
-    -- Check that given proc exists for actual operation type.
-    -- Cast to regprocedure fails if it doesn't.
-    -- In that case we catch it t not propagate it to the caller
-    PERFORM (format('%I(hive.%I)', proc, __operation_type))::regprocedure;
-
-    -- Call user provided function
+  -- Call user provided function, if it exists for given operation type
+  IF (SELECT to_regprocedure(format('%I(hive.%I)', proc, __operation_type))) IS NOT NULL THEN
     EXECUTE format('CALL %I($1::hive.%I)', proc, __operation_type)
       USING operation;
-  EXCEPTION
-    WHEN undefined_function THEN RETURN;
-  END;
+  END IF;
 END;
 $BODY$;
