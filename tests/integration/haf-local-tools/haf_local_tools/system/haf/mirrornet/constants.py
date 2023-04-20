43fdc5dccaf2b49e0519b5947a3f25fe2a4c3865
Radek Masłowski <rmaslowski@syncad.com>
2023-04-20 08:21:28 +0000

    Add constants and help functions

---
+++ tests/integration/haf-local-tools/haf_local_tools/system/haf/mirrornet/constants.py
@@ -0,0 +1,42 @@
+CHAIN_ID: str = "42"
+SKELETON_KEY: str = "5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n"
+
+TRANSACTION_IN_1092_BLOCK: str = "7c45893e5a16bbb63f3adbbe9ccd53547a8e50aa"
+TRANSACTION_IN_999892_BLOCK: str = "d646c2c852d60c6afee0faa38c3d28d4477b3730"
+TRANSACTION_IN_4000000_BLOCK: str = "ba561cc93419bce2d067e7820a19f4ce9c5ec84b"
+TRANSACTION_IN_4000001_BLOCK: str = "b740c81e370a26f0667ce97015b9420cd6b489a5"
+TRANSACTION_IN_5000000_BLOCK: str = "a789b01861767e64dd1a6f4984e316287a706623"
+
+WITNESSES_5M: list[str] = [
+    "rabbit-70",
+    "kushed",
+    "delegate.lafona",
+    "wackou",
+    "complexring",
+    "jesta",
+    "xeldal",
+    "riverhead",
+    "clayop",
+    "steemed",
+    "smooth.witness",
+    "ihashfury",
+    "joseph",
+    "datasecuritynode",
+    "boatymcboatface",
+    "steemychicken1",
+    "roadscape",
+    "pharesim",
+    "abit",
+    "blocktrades",
+    "arhag",
+    "bitcube",
+    "witness.svk",
+    "gxt-1080-sc-0003",
+    "steve-walschot",
+    "bhuz",
+    "liondani",
+    "rabbit-63",
+    "pfunk",
+]
+
+TIMESTAMP_5M: str = "@2016-09-15 19:47:24"
