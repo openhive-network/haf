28d48f4e25c1d2b5c4e2376a0190f1cad0bfc14e
Radek Masłowski <rmaslowski@syncad.com>
2023-05-09 13:17:37 +0000

    fixup! Add simple test

--- tests/integration/system/haf/mirrornet_tests/simple_test.py
+++ tests/integration/system/haf/mirrornet_tests/simple_test.py
@@ -7,6 +7,6 @@ import pytest
 def test_simple(tmp_path, block_log_5m_path):
     witness_node = tt.WitnessNode()
     block_log_5m = tt.BlockLog(block_log_5m_path)
-    block_log_100k = block_log_5m.truncate(tmp_path, 100000, False)
+    block_log_100k = block_log_5m.truncate(tmp_path, 100000)
 
-    witness_node.run(replay_from=block_log_100k, wait_for_live=False)
+    witness_node.run(replay_from=block_log_100k, wait_for_live=True)
