47069f815ba4f8719c0c0a3a8469034a99a9a572
Marcin Ickiewicz <mickiewicz@syncad.com>
2023-05-11 09:48:21 +0200

    contexts array app next block for non forking app

--- src/hive_fork_manager/app_api_impl.sql
+++ src/hive_fork_manager/app_api_impl.sql
@@ -422,7 +422,7 @@ END;
 $BODY$
 ;
 
-CREATE OR REPLACE FUNCTION hive.app_next_block_non_forking_app( _context_name TEXT )
+CREATE OR REPLACE FUNCTION hive.app_next_block_non_forking_app( _context_names TEXT[] )
     RETURNS hive.blocks_range
     LANGUAGE plpgsql
     VOLATILE
@@ -432,14 +432,31 @@ DECLARE
     __result hive.blocks_range;
     __context_state hive.context_state;
 BEGIN
-    SELECT * FROM hive.squash_and_get_state( _context_name ) INTO __context_state;
-    SELECT * FROM hive.app_process_event_non_forking( _context_name, __context_state ) INTO __result;
+    ASSERT array_length( _context_names, 1 ) > 0, 'Empty array of contexts';
+
+    SELECT * FROM hive.squash_and_get_state( _context_names[1] ) INTO __context_state;
+    SELECT * FROM hive.app_process_event_non_forking( _context_names[1], __context_state ) INTO __result;
 
     IF __result.first_block > __result.last_block THEN
         PERFORM pg_sleep( 1.5 );
         RETURN NULL;
     END IF;
 
+    RETURN __result;
+END;
+$BODY$
+;
+
+CREATE OR REPLACE FUNCTION hive.app_next_block_non_forking_app( _context_name TEXT )
+    RETURNS hive.blocks_range
+    LANGUAGE plpgsql
+    VOLATILE
+AS
+$BODY$
+DECLARE
+    __result hive.blocks_range;
+BEGIN
+    SELECT * FROM hive.app_next_block_non_forking_app( ARRAY[ _context_name ] ) INTO __result;
     RETURN __result;
 END;
 $BODY$
