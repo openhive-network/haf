df231e1e2ffe4097d65301d344ab5157c447bd8e
mkrzeminski <mkrzeminski@syncad.com>
2023-06-15 03:28:37 +0000

    Dynamic - DIP

--- src/hive_fork_manager/tools.sql
+++ src/hive_fork_manager/tools.sql
@@ -19,3 +19,27 @@ BEGIN
 END;
 $BODY$
 ;
+
+CREATE OR REPLACE FUNCTION hive.get_postgres_url()
+    RETURNS TEXT
+    LANGUAGE plpgsql
+    VOLATILE
+AS
+$BODY$
+DECLARE
+    __current_pid INT;
+    __database_name TEXT;
+    __postgres_url TEXT;
+BEGIN
+    __current_pid := pg_backend_pid();
+    SELECT datname AS database_name
+    FROM pg_stat_activity
+    WHERE pid = __current_pid INTO __database_name;
+
+    __postgres_url := 'postgres:///' || __database_name;
+    RAISE NOTICE '__postgres_url=%', __postgres_url;
+    RETURN __postgres_url;
+END;
+$BODY$
+;
+
