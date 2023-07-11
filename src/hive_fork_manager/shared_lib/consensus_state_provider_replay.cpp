5fd961fda973336488363b294fa1db879c9c0d48
mkrzeminski <mkrzeminski@syncad.com>
2023-07-11 11:13:16 +0000

    is_included_block_unlocked up

--- src/hive_fork_manager/shared_lib/consensus_state_provider_replay.cpp
+++ src/hive_fork_manager/shared_lib/consensus_state_provider_replay.cpp
@@ -71,7 +71,6 @@ private:
   void migrate_irreversible_state(uint32_t old_last_irreversible) override{myASSERT(1, "STOP mtlk");}
   
   std::vector<block_id_type> get_blockchain_synopsis(const block_id_type& reference_point, uint32_t number_of_blocks_after_reference_point) override{myASSERT(0, "STOP mtlk");}
-  bool is_included_block_unlocked(const block_id_type& block_id) override{myASSERT(0, "STOP mtlk");}
   std::vector<block_id_type> get_block_ids(const std::vector<block_id_type>& blockchain_synopsis, uint32_t& remaining_item_count, uint32_t limit) override{myASSERT(0, "STOP mtlk");}
 
   std::shared_ptr<full_block_type> get_head_block() const override;
