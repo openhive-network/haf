8fe8a73d4d2dc5280209ef0a8ea93576f128e039
Mateusz Kudela <mkudela@syncad.com>
2023-08-16 15:00:43 +0200

    TEST COMMIT TEST COMMIT TEST COMMIT

--- tests/integration/system/haf/mirrornet_tests/test_p2p_sync_in_mirrornet.py
+++ tests/integration/system/haf/mirrornet_tests/test_p2p_sync_in_mirrornet.py
@@ -32,22 +32,23 @@ def test_p2p_sync(
     block_log_1m = block_log_5m.truncate(tmp_path, 1000000)
 
     mirrornet_witness_node.run(
-        replay_from=block_log_1m,
-        time_offset=TIMESTAMP_5M,
-        wait_for_live=True,
+        replay_from=block_log_5m,
+        exit_before_synchronization=True,
         timeout=3600,
         arguments=["--chain-id", CHAIN_ID, "--skeleton-key", SKELETON_KEY],
     )
 
-    time_offset = tt.Time.serialize(
-        time=mirrornet_witness_node.get_head_block_time(),
-        format_=tt.Time.TIME_OFFSET_FORMAT,
-    )
 
     connect_nodes(mirrornet_witness_node, haf_node)
 
+    mirrornet_witness_node.run(
+        time_offset=TIMESTAMP_5M, 
+        wait_for_live=True, 
+        timeout=3600,
+        arguments=["--chain-id", CHAIN_ID, "--skeleton-key", SKELETON_KEY],)
+    
     haf_node.run(
-        time_offset=time_offset,
+        time_offset=TIMESTAMP_5M,
         wait_for_live=True,
         timeout=3600,
         arguments=["--chain-id", CHAIN_ID],
