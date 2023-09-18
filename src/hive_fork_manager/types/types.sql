27eca6e44c9b3a541ca2b7fc10d23b07817b7832
Krzysztof Leśniak <klesniak@syncad.com>
2023-09-18 15:45:42 +0200

    Remove extra fields from proposal_pay_operation

--- src/hive_fork_manager/types/types.sql
+++ src/hive_fork_manager/types/types.sql
@@ -518,9 +518,7 @@ CREATE TYPE hive.proposal_pay_operation AS (
   proposal_id int8, -- uint32_t: 4 bytes, but unsigned (int8)
   receiver hive.account_name_type,
   payer hive.account_name_type,
-  payment hive.asset,
-  trx_id hive.transaction_id_type,
-  op_in_trx int4 -- uint16_t: 2 bytes, but unsigned (int4)
+  payment hive.asset
 );
 
 CREATE TYPE hive.remove_proposal_operation AS (
