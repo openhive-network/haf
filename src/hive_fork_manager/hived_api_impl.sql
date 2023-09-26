c4c5186dc92c981b50701ef6c1368a66a5965bf4
mkrzeminski <mkrzeminski@syncad.com>
2023-09-26 21:13:35 +0200

    Move foreign constraints out of table creation

--- src/hive_fork_manager/hived_api_impl.sql
+++ src/hive_fork_manager/hived_api_impl.sql
@@ -390,10 +390,7 @@ BEGIN
     SELECT
           DISTINCT ON ( pgc.conname ) pgc.conname as constraint_name
         , _table_schema || '.' || _table_name as table_name
-        , CASE WHEN position('NOT VALID' in pg_get_constraintdef(pgc.oid)) > 0 
-        THEN 'ALTER TABLE ' || tc.table_schema || '.' || tc.table_name || ' ADD CONSTRAINT ' || pgc.conname || ' ' || pg_get_constraintdef(pgc.oid)
-        ELSE 'ALTER TABLE ' || tc.table_schema || '.' || tc.table_name || ' ADD CONSTRAINT ' || pgc.conname || ' ' || pg_get_constraintdef(pgc.oid) || ' ' || 'NOT VALID' 
-        END as command
+        , 'ALTER TABLE ' || tc.table_schema || '.' || tc.table_name || ' ADD CONSTRAINT ' || pgc.conname || ' ' || pg_get_constraintdef(pgc.oid) as command
         , FALSE as is_constraint
         , FALSE AS is_index
         , TRUE as is_foreign_key
