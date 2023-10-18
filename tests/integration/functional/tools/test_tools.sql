f3cac733b3f2bcd6bf9272af53f6f7ea6cddbf17
Krzysztof Leśniak <klesniak@syncad.com>
2023-10-18 13:34:28 +0200

    Introduced hive.check_eq

--- tests/integration/functional/tools/test_tools.sql
+++ tests/integration/functional/tools/test_tools.sql
@@ -11,3 +11,16 @@ BEGIN
 END
 $$
 ;
+
+CREATE PROCEDURE hive.check_eq(a anyelement, b anyelement, msg text DEFAULT 'Expected to be equal, but failed')
+LANGUAGE plpgsql
+AS
+$BODY$
+BEGIN
+  IF a <> b THEN
+    assert (SELECT FALSE), FORMAT(E'%s:\na: %s\nb: %s', msg, a, b);
+  END IF;
+END;
+$BODY$
+;
+
