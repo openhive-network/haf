ab3e20b388e45d67809a200226179e534c9ab50d
mkrzeminski <mkrzeminski@syncad.com>
2023-10-23 09:55:56 +0200

    Keyauth: Remove functions no longer used
    is_keyauths_operation,
    is_metadata_operation
    vector_to_string_array_datum

--- src/hive_fork_manager/get_metadata.sql
+++ src/hive_fork_manager/get_metadata.sql
@@ -21,8 +21,3 @@ DROP FUNCTION IF EXISTS hive.get_metadata_operations;
 CREATE OR REPLACE FUNCTION hive.get_metadata_operations()
 RETURNS SETOF hive.get_metadata_operations_type
 AS 'MODULE_PATHNAME', 'get_metadata_operations' LANGUAGE C;
-
-DROP FUNCTION IF EXISTS hive.is_metadata_operation;
-CREATE OR REPLACE FUNCTION hive.is_metadata_operation(IN _operation_body hive.operation)
-RETURNS Boolean
-AS 'MODULE_PATHNAME', 'is_metadata_operation' LANGUAGE C;
