501e8c3b61c5128f74089331f263ace0d7a3af9e
Marcin Ickiewicz <mickiewicz@syncad.com>
2023-10-31 09:39:59 +0100

    system tests for syncing not from 1st block

---
+++ tests/integration/system/haf/test_live_sync_from_115.py
@@ -0,0 +1,46 @@
+import json
+
+from sqlalchemy import cast
+from sqlalchemy.dialects.postgresql import JSONB
+
+import test_tools as tt
+
+from haf_local_tools import get_head_block, get_irreversible_block, wait_for_irreversible_progress
+from haf_local_tools.tables import Blocks, BlocksView, Transactions, Operations
+
+
+START_TEST_BLOCK =  115
+
+
+def test_live_sync_from_115(prepared_networks_and_database_12_8_from_115):
+    tt.logger.info(f'Start test_live_sync_from_115')
+
+    # GIVEN
+    networks_builder, session = prepared_networks_and_database_12_8_from_115
+    witness_node = networks_builder.networks[0].node('WitnessNode0')
+    node_under_test = networks_builder.networks[1].node('ApiNode0')
+
+    # WHEN
+    node_under_test.wait_for_block_with_number(START_TEST_BLOCK)
+    wallet = tt.Wallet(attach_to=witness_node)
+    wallet.api.transfer('initminer', 'initminer', tt.Asset.Test(1000), 'dummy transfer operation')
+    transaction_block_num = START_TEST_BLOCK + 1
+
+    # THEN
+    wait_for_irreversible_progress(node_under_test, transaction_block_num)
+    head_block = get_head_block(node_under_test)
+    irreversible_block = get_irreversible_block(node_under_test)
+
+    blks = session.query(Blocks).order_by(Blocks.num).all()
+    block_nums = [block.num for block in blks]
+    assert sorted(block_nums)[:2] == [i for i in [0, 115]]
+
+    tt.logger.info(f'head_block: {head_block} irreversible_block: {irreversible_block}')
+
+    session.query(Transactions).filter(Transactions.block_num == transaction_block_num).one()
+
+    ops = session.query(Operations).add_columns(cast(Operations.body_binary, JSONB).label('body'), Operations.block_num).filter(Operations.block_num == transaction_block_num).all()
+    types = [op.body['type'] for op in ops]
+
+    assert 'transfer_operation' in types
+    assert 'producer_reward_operation' in types
--- tests/integration/system/haf/test_p2p_sync_from_4.py
+++ tests/integration/system/haf/test_p2p_sync_from_4.py
@@ -7,19 +7,22 @@ from haf_local_tools.system.haf import (
     assert_are_indexes_restored,
     connect_nodes,
     prepare_and_send_transactions,
+    BlocksView
 )
-
 
 @pytest.mark.parametrize(
     "psql_index_threshold",
     [2147483647, 100000],
     ids=["enabled_indexes", "disabled_indexes_in_p2p_sync"],
 )
-def test_p2p_sync(haf_node, psql_index_threshold):
+def test_p2p_sync_from_4(haf_node, psql_index_threshold):
     init_node = tt.InitNode()
     init_node.run()
 
     haf_node.config.psql_index_threshold = psql_index_threshold
+    haf_node.config.psql_first_block = 4
+
+    init_node.wait_number_of_blocks(3)
 
     transaction_0, transaction_1 = prepare_and_send_transactions(init_node)
 
@@ -32,6 +35,13 @@ def test_p2p_sync(haf_node, psql_index_threshold):
     haf_node.wait_for_transaction_in_database(transaction_0)
     haf_node.wait_for_transaction_in_database(transaction_1)
 
-    assert_are_blocks_sync_with_haf_db(haf_node, transaction_1["block_num"])
-    assert_are_indexes_restored(haf_node)
+    # syncing has started 3 block later than blockchain start block
+    block_of_transaction1 = transaction_1["block_num"]
+    blocks_in_database_before_transaction1 = (
+        haf_node.session.query(BlocksView).filter(BlocksView.num <= block_of_transaction1).count()
+    )
 
+    tt.logger.info(f"assert_are_blocks_sync_with_haf_db actual {blocks_in_database_before_transaction1}, expected {block_of_transaction1-3}")
+    assert blocks_in_database_before_transaction1 == block_of_transaction1 - 3
+
+    assert_are_indexes_restored(haf_node)
