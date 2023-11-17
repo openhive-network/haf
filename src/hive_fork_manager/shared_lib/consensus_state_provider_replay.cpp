eebad832cc1ff57aa720349885c7edeaba4b8b53
mkrzeminski <mkrzeminski@syncad.com>
2023-11-17 23:30:53 +0100

    no use of head reader in haf

--- src/hive_fork_manager/shared_lib/consensus_state_provider_replay.cpp
+++ src/hive_fork_manager/shared_lib/consensus_state_provider_replay.cpp
@@ -5,6 +5,8 @@
 #include <fc/io/sstream.hpp>
 #include "fc/time.hpp"
 #include <hive/plugins/block_api/block_api_objects.hpp>
+#include <hive/chain/full_database.hpp>
+
 
 #include <pqxx/pqxx>
 
@@ -25,7 +27,7 @@ using full_block_ptr = std::shared_ptr<full_block_type>;
 
 
 //This is the override class of the chain::database used in consensus_state_provide - more info mtlk todo
-class haf_state_database : public hive::chain::database
+class haf_state_database : public hive::chain::full_database
 {
 public:
   haf_state_database(csp_session_ref_type csp_session):csp_session(csp_session){}
@@ -449,10 +451,10 @@ void haf_state_database::apply_haf_block(const full_block_ptr& full_block, uint3
 void haf_state_database::state_dependent_open( const open_args& args )
 {
   load_state_initial_data(args,
-    [this](uint32_t block_num)
+    [this](uint32_t block_num) -> std::shared_ptr<full_block_type>
     {
-      auto full_block = postgres_block_log(csp_session).read_full_block(head_block_num());
-      return full_block;
+      //auto full_block = postgres_block_log(csp_session).read_full_block(head_block_num());
+      return {};
     });
 }
 
