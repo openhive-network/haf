0af5d7c78e91a4c97e5b7c4a40115436bca36a15
mkrzeminski <mkrzeminski@syncad.com>
2023-11-20 15:55:17 +0100

    another dump

--- src/hive_fork_manager/state_providers/keyauth.sql
+++ src/hive_fork_manager/state_providers/keyauth.sql
@@ -242,7 +242,8 @@ BEGIN
         ),
 
         from_combined_select AS (
-            SELECT *  FROM combined_data
+            SELECT *  FROM hive.%1$s_keyauth_a
+            WHERE (account_id, key_kind) IN (SELECT account_id, key_kind FROM combined_data)
         ),
 
         -- Deletes existing keyauth_a records to be replaced with updated data.
