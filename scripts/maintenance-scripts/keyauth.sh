c7645c97e02aeb365aedb3947157095c7bed4d01
Michal Zander <mzander@syncad.com>
2023-12-06 13:17:47 +0000

    Allows testing locally

--- scripts/maintenance-scripts/keyauth.sh
+++ scripts/maintenance-scripts/keyauth.sh
@@ -9,37 +9,81 @@ LOG_FILE=replay_with_keyauths.log
 
 source "$SCRIPTSDIR/maintenance-scripts/ci_common.sh"
 
-test_start
+#test_start
 
 # container must have /blockchain directory mounted containing block_log with at 5000000 first blocks
-export BLOCK_LOG_SOURCE_DIR_5M="/blockchain/block_log_5m"
-export PATTERNS_PATH="${REPO_DIR}/tests/integration/replay/patterns/no_filter"
-export DATADIR="${REPO_DIR}/datadir"
-export REPLAY="--replay-blockchain --stop-replay-at-block 5000000"
-export HIVED_PATH="/home/hived/bin/hived"
-export COMPRESS_BLOCK_LOG_PATH="/home/hived/bin/compress_block_log"
+
 export DB_NAME=haf_block_log
 export DB_ADMIN="haf_admin"
-export SETUP_SCRIPTS_PATH="/home/haf_admin/haf/scripts"
 
-echo -e "\e[0Ksection_start:$(date +%s):replay[collapsed=true]\r\e[0KExecuting replay..."
-test -n "$PATTERNS_PATH"
-mkdir $DATADIR/blockchain -p
-cp "$PATTERNS_PATH"/* "$DATADIR" -r
-cp ${BLOCK_LOG_SOURCE_DIR_5M}/block_log $DATADIR/blockchain
-$HIVED_PATH --data-dir "$DATADIR" $REPLAY --exit-before-sync --psql-url "postgresql:///$DB_NAME" 2>&1 | tee -i node_logs.log
-echo -e "\e[0Ksection_end:$(date +%s):replay\r\e[0K"
+print_help () {
+cat <<EOF
+  Usage: $0 [OPTION[=VALUE]]...
+
+  Processes blocks using Haf Block Explorer
+  OPTIONS:
+    --host=HOST             PostgreSQL host (defaults to localhost)
+    --port=PORT             PostgreSQL operating port (defaults to 5432)
+    --user=USER             PostgreSQL username (defaults to hafbe_owner)
+    --limit=LIMIT           Max number of blocks to process (0 for infinite, defaults to 0)
+    --log-file              Log file location (defaults to hafbe_sync.log, set to 'STDOUT' to log to STDOUT only)
+EOF
+}
+
+POSTGRES_HOST=${POSTGRES_HOST:-"localhost"}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_USER=${POSTGRES_USER:-"haf_admin"}
+PROCESS_BLOCK_LIMIT=${PROCESS_BLOCK_LIMIT:-0}
+LOG_FILE=${LOG_FILE:-"hafbe_sync.log"}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --host=*)
+        POSTGRES_HOST="${1#*=}"
+        ;;
+    --port=*)
+        POSTGRES_PORT="${1#*=}"
+        ;;
+    --user=*)
+        POSTGRES_USER="${1#*=}"
+        ;;
+    --limit=*)
+        PROCESS_BLOCK_LIMIT="${1#*=}"
+        ;;
+    --log-file=*)
+        LOG_FILE="${1#*=}"
+        ;;
+    --help|-h|-?)
+        print_help
+        exit 0
+        ;;
+    -*)
+        echo "ERROR: '$1' is not a valid option"
+        echo
+        print_help
+        exit 1
+        ;;
+    *)
+        echo "ERROR: '$1' is not a valid argument"
+        echo
+        print_help
+        exit 2
+        ;;
+    esac
+    shift
+done
+
+POSTGRES_ACCESS="postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/haf_block_log"
 
 
-psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "SELECT hive.app_create_context('keyauth_live');"
-psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "SELECT hive.app_state_provider_import('KEYAUTH', 'keyauth_live');"
+
 
 echo "Replay of keyauths..."
 bash "${SCRIPTPATH}/keyauths_comparison/process_klive.sh"
 
 echo "Clearing tables..."
-psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "TRUNCATE keyauth_live.keys;"
-psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "TRUNCATE keyauth_live.differing_accounts;"
+psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -c "TRUNCATE keyauth_live.keys;"
+psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -c"TRUNCATE keyauth_live.differing_accounts;"
 
 echo "Installing dependecies..."
 pip install psycopg2-binary
@@ -55,20 +99,20 @@ rm -f "${SCRIPTPATH}/keyauths_comparison/accounts_dump.json"
 gunzip -k "${SCRIPTPATH}/keyauths_comparison/accounts_dump.json.gz"
 
 echo "Starting data_insertion_script.py..."
-python3 $SCRIPTPATH/keyauths_comparison/data_insertion_script.py "$SCRIPTPATH"/keyauths_comparison --host="/var/run/postgresql" #--debug
+python3 $SCRIPTPATH/keyauths_comparison/data_insertion_script.py "$SCRIPTPATH"/keyauths_comparison --host="172.17.0.2" #--debug
 
 echo "Looking for diffrences between hived node and keyauths..."
-psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "SELECT keyauth_live.compare_accounts();"
+psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -c "SELECT keyauth_live.compare_accounts();"
 
-DIFFERING_ACCOUNTS=$(psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -t -A -c "SELECT * FROM keyauth_live.differing_accounts;")
+DIFFERING_ACCOUNTS=$(psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -t -A -c "SELECT * FROM keyauth_live.differing_accounts;")
 
 if [ -z "$DIFFERING_ACCOUNTS" ]; then
     echo "keyauths are correct!"
     exit 0
 else
     echo "keyauths are incorrect..."
-    psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "SELECT * FROM keyauth_live.differing_accounts;"
+    psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -c "SELECT * FROM keyauth_live.differing_accounts;"
     exit 3
 fi
 
-test_end
+#test_end
--- scripts/maintenance-scripts/keyauths_comparison/process_klive.sh
+++ scripts/maintenance-scripts/keyauths_comparison/process_klive.sh
@@ -8,9 +8,71 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export DB_NAME=haf_block_log
 export DB_ADMIN="haf_admin"
 
+print_help () {
+cat <<EOF
+  Usage: $0 [OPTION[=VALUE]]...
+
+  Processes blocks using Haf Block Explorer
+  OPTIONS:
+    --host=HOST             PostgreSQL host (defaults to localhost)
+    --port=PORT             PostgreSQL operating port (defaults to 5432)
+    --user=USER             PostgreSQL username (defaults to hafbe_owner)
+    --limit=LIMIT           Max number of blocks to process (0 for infinite, defaults to 0)
+    --log-file              Log file location (defaults to hafbe_sync.log, set to 'STDOUT' to log to STDOUT only)
+EOF
+}
+
+POSTGRES_HOST=${POSTGRES_HOST:-"172.17.0.2"}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_USER=${POSTGRES_USER:-"haf_admin"}
+PROCESS_BLOCK_LIMIT=${PROCESS_BLOCK_LIMIT:-0}
+LOG_FILE=${LOG_FILE:-"hafbe_sync.log"}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --host=*)
+        POSTGRES_HOST="${1#*=}"
+        ;;
+    --port=*)
+        POSTGRES_PORT="${1#*=}"
+        ;;
+    --user=*)
+        POSTGRES_USER="${1#*=}"
+        ;;
+    --limit=*)
+        PROCESS_BLOCK_LIMIT="${1#*=}"
+        ;;
+    --log-file=*)
+        LOG_FILE="${1#*=}"
+        ;;
+    --help|-h|-?)
+        print_help
+        exit 0
+        ;;
+    -*)
+        echo "ERROR: '$1' is not a valid option"
+        echo
+        print_help
+        exit 1
+        ;;
+    *)
+        echo "ERROR: '$1' is not a valid argument"
+        echo
+        print_help
+        exit 2
+        ;;
+    esac
+    shift
+done
+
+POSTGRES_ACCESS="postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/haf_block_log"
+
+
+
+
 process_blocks() {
-    psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -f "${SCRIPTDIR}/keyauth_live_schema.sql"
-    psql -w -d $DB_NAME -v ON_ERROR_STOP=on -U $DB_ADMIN -c "CALL keyauth_live.main('keyauth_live', 0, 5000000, 500000);" 
+    #psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on"  -f "${SCRIPTDIR}/keyauth_live_schema.sql"
+    psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -c "CALL keyauth_live.main('keyauth_live', 0, 5000000, 500000);" 
 }
 
 
