64ae946dd90dbc28488fdfb44888b95e758d4da3
Bartek Wrona <wrona@syncad.com>
2023-12-09 19:58:08 +0100

    sink_id function definitions moved into separate source file to apply their definitions also on upgrade.

---
+++ src/hive_fork_manager/context_rewind/sink_id_functions.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE FUNCTION hive.unreachable_event_id()
+    RETURNS BIGINT
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$$
+BEGIN
+    RETURN 9223372036854775807; -- MAX BIGINT
+END
+$$;
+
+CREATE OR REPLACE FUNCTION hive.block_sink_num()
+    RETURNS INT
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$$
+BEGIN
+    RETURN 0;
+END
+$$;
+
+CREATE OR REPLACE FUNCTION hive.account_sink_id()
+    RETURNS INT
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$$
+BEGIN
+    RETURN -1;
+END
+$$;
