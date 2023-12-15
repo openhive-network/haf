aebee7eef3cb247c27e298c4778815715034c7f6
Eric Frias <efrias@syncad.com>
2023-12-15 00:31:55 +0000

    Update string traits for pqxx 7.x

--- src/sql_serializer/include/hive/plugins/sql_serializer/container_data_writer.h
+++ src/sql_serializer/include/hive/plugins/sql_serializer/container_data_writer.h
@@ -11,59 +11,342 @@
 
 namespace pqxx {
   namespace {
-    std::string escape_binary(std::string_view binary_str)
-    { 
-      std::string result;
-      result.resize(binary_str.size() * 2 + 2);
-      unsigned pos = 0;
-      result[pos++] = '\\';
-      result[pos++] = 'x';
-      constexpr const char* to_hex = "0123456789abcdef";
-      for (const char c : binary_str)
-      {
-        result[pos++] = to_hex[(unsigned char)c >> 4];
-        result[pos++] = to_hex[(unsigned char)c & 0x0f];
-      }
-      return result;
+    // these helper functions are copied straight from pqxx, which marked them as "internal"
+    inline constexpr std::size_t size_esc_bin(std::size_t binary_bytes) noexcept
+    {
+      return 2 + (2 * binary_bytes) + 1;
     }
+    constexpr char hex_digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    // Translate a number (must be between 0 and 16 exclusive) to a hex digit.
+    constexpr char hex_digit(int c) noexcept
+    {
+      return hex_digits[c];
+    }
+    // Translate a hex digit to a nibble.  Return -1 if it's not a valid digit.
+    constexpr int nibble(int c) noexcept
+    {
+      if (c >= '0' && c <= '9')
+        [[likely]] return c - '0';
+      else if (c >= 'a' && c <= 'f')
+        return 10 + (c - 'a');
+      else if (c >= 'A' && c <= 'F')
+        return 10 + (c - 'A');
+      else
+        return -1;
+    }
+
+    void esc_bin(std::basic_string_view<std::byte> binary_data, char buffer[]) noexcept
+    {
+      auto here{buffer};
+      *here++ = '\\';
+      *here++ = 'x';
+
+      for (auto const byte : binary_data)
+      {
+        auto uc{static_cast<unsigned char>(byte)};
+        *here++ = hex_digit(uc >> 4);
+        *here++ = hex_digit(uc & 0x0f);
+      }
+
+      *here = '\0';
+    }
+    inline constexpr std::size_t size_unesc_bin(std::size_t escaped_bytes) noexcept
+    {
+      return (escaped_bytes - 2) / 2;
+    }
+    void unesc_bin(std::string_view escaped_data, std::byte buffer[])
+    {
+      auto const in_size{std::size(escaped_data)};
+      if (in_size < 2)
+        throw pqxx::failure{"Binary data appears truncated."};
+      if ((in_size % 2) != 0)
+        throw pqxx::failure{"Invalid escaped binary length."};
+      char const* in{escaped_data.data()};
+      char const* const end{in + in_size};
+      if (*in++ != '\\' || *in++ != 'x')
+        throw pqxx::failure("Escaped binary data did not start with '\\x'`.  Is the server or libpq "
+                            "too old?");
+      auto out{buffer};
+      while (in != end)
+      {
+        int hi{nibble(*in++)};
+        if (hi < 0)
+          throw pqxx::failure{"Invalid hex-escaped data."};
+        int lo{nibble(*in++)};
+        if (lo < 0)
+          throw pqxx::failure{"Invalid hex-escaped data."};
+        *out++ = static_cast<std::byte>((hi << 4) | lo);
+      }
+    }
+  } // end anonymous namespace for helper functions
+
+// support for fc::optional<>, adapted from pqxx's existing support for std::optional
+template <typename T> struct nullness<fc::optional<T>>
+{
+  static constexpr bool has_null = true;
+  static constexpr bool always_null = nullness<T>::always_null;
+  static constexpr bool is_null(fc::optional<T> const& v) noexcept { return ((!v.valid()) || pqxx::is_null(*v)); }
+  static constexpr fc::optional<T> null() { return {}; }
+};
+
+template <typename T> inline constexpr format param_format(fc::optional<T> const& value) { return param_format(*value); }
+
+template <typename T> struct string_traits<fc::optional<T>>
+{
+  static constexpr bool converts_to_string{string_traits<T>::converts_to_string};
+  static constexpr bool converts_from_string{string_traits<T>::converts_from_string};
+
+  static char* into_buf(char* begin, char* end, fc::optional<T> const& value) { return string_traits<T>::into_buf(begin, end, *value); }
+
+  static zview to_buf(char* begin, char* end, fc::optional<T> const& value)
+  {
+    if (value.valid())
+      return string_traits<T>::to_buf(begin, end, *value);
+    else
+      return {};
   }
-  template<> struct string_traits<binarystring>
+
+  static fc::optional<T> from_string(std::string_view text) { return fc::optional<T>{std::in_place, string_traits<T>::from_string(text)}; }
+
+  static std::size_t size_buffer(fc::optional<T> const& value) noexcept { return pqxx::size_buffer(value.value()); }
+};
+
+// ripemd160 to BYTEA, (for block hashes)
+template<> std::string const type_name<fc::ripemd160>{"fc::ripemd160"};
+template<> struct nullness<fc::ripemd160> : pqxx::no_null<fc::ripemd160> {};
+
+template<> struct string_traits<fc::ripemd160>
+{
+  static constexpr bool converts_to_string{true};
+  static constexpr bool converts_from_string{true};
+
+  static zview to_buf(char *begin, char *end, const fc::ripemd160& value)
   {
-    static constexpr const char *name() noexcept { return "binarystring"; }
-    static constexpr bool has_null() noexcept { return false; }
-    static bool is_null(const binarystring&) { return false; }
-    [[noreturn]] static binarystring null() { internal::throw_null_conversion(name()); }
-    static void from_string(const char Str[], binarystring &Obj) { FC_ASSERT(false, "not implemented"); }
-    static std::string to_string(const binarystring& binary_str) { return escape_binary(std::string_view((const char*)binary_str.data(), binary_str.size())); }
-  };
-  template<> struct string_traits<fc::time_point_sec>
+    // we can't optimize this any more, just call into_buf()
+    return generic_to_buf(begin, end, value);
+  }
+
+  static char *into_buf(char *begin, char *end, const fc::ripemd160& value)
   {
-    static constexpr const char *name() noexcept { return "fc::time_point_sec"; }
-    static constexpr bool has_null() noexcept { return false; }
-    static bool is_null(const fc::time_point_sec&) { return false; }
-    [[noreturn]] static fc::time_point_sec null() { internal::throw_null_conversion(name()); }
-    static void from_string(const char Str[], fc::time_point_sec &Obj) { FC_ASSERT(false, "not implemented"); }
-    static std::string to_string(const fc::time_point_sec& time_point) { return time_point.to_iso_string(); }
-  };
-  template<> struct string_traits<fc::ripemd160>
+    if (end - begin < (long)size_buffer(value))
+      throw conversion_overrun{"Could not write fc::ripemd160: buffer too small."};
+    esc_bin(binary_cast(value.data(), value.data_size()), begin);
+    return begin + size_buffer(value);
+  }
+
+  static constexpr std::size_t size_buffer(const fc::ripemd160&) noexcept { return size_esc_bin(sizeof(fc::ripemd160)); }
+
+  static fc::ripemd160 from_string(std::string_view text)
   {
-    static constexpr const char *name() noexcept { return "fc::ripemd160"; }
-    static constexpr bool has_null() noexcept { return false; }
-    static bool is_null(const fc::ripemd160&) { return false; }
-    [[noreturn]] static fc::ripemd160 null() { internal::throw_null_conversion(name()); }
-    static void from_string(const char Str[], fc::ripemd160 &Obj) { FC_ASSERT(false, "not implemented"); }
-    static std::string to_string(const fc::ripemd160& hash) { return escape_binary(std::string_view(hash.data(), hash.data_size())); }
-  };
-  template<> struct string_traits<fc::ecc::compact_signature>
+    fc::ripemd160 result;
+    unesc_bin(text, reinterpret_cast<std::byte*>(result.data()));
+    return result;
+  }
+};
+
+
+// signatures to BYTEA
+template<> std::string const type_name<fc::ecc::compact_signature>{"fc::ecc::compact_signature"};
+template<> struct nullness<fc::ecc::compact_signature> : pqxx::no_null<fc::ecc::compact_signature> {};
+
+template<> struct string_traits<fc::ecc::compact_signature>
+{
+  static constexpr bool converts_to_string{true};
+  static constexpr bool converts_from_string{true};
+
+  static zview to_buf(char *begin, char *end, const fc::ecc::compact_signature& value)
   {
-    static constexpr const char *name() noexcept { return "fc::ecc::compact_signature"; }
-    static constexpr bool has_null() noexcept { return false; }
-    static bool is_null(const fc::ecc::compact_signature&) { return false; }
-    [[noreturn]] static fc::ecc::compact_signature null() { internal::throw_null_conversion(name()); }
-    static void from_string(const char Str[], fc::ecc::compact_signature &Obj) { FC_ASSERT(false, "not implemented"); }
-    static std::string to_string(const fc::ecc::compact_signature& sig) { return escape_binary(std::string_view((const char*)sig.begin(), sig.size())); }
-  };
-}
+    // we can't optimize this any more, just call into_buf()
+    return generic_to_buf(begin, end, value);
+  }
+
+  static char *into_buf(char *begin, char *end, const fc::ecc::compact_signature& value)
+  {
+    if (end - begin < (long)size_buffer(value))
+      throw conversion_overrun{"Could not write fc::ecc::compact_signature: buffer too small."};
+    esc_bin(binary_cast(value.begin(), value.size()), begin);
+    return begin + size_buffer(value);
+  }
+
+  static constexpr std::size_t size_buffer(const fc::ecc::compact_signature&) noexcept { return size_esc_bin(sizeof(fc::ecc::compact_signature)); }
+
+  static fc::ecc::compact_signature from_string(std::string_view text)
+  {
+    fc::ecc::compact_signature result;
+    unesc_bin(text, reinterpret_cast<std::byte*>(result.begin()));
+    return result;
+  }
+};
+
+// conversion for time_point_sec into an ISO8601 string like "2023-12-13T17:26:12" for
+// PostgreSQL.
+// *NOTE* right now this doesn't add a trailing "Z" because the haf database
+// incorrectly uses "timestamp without time zone" everywhere.  if that is fixed,
+// this converter will need to be updated to add a "Z" at the end.
+template<> std::string const type_name<fc::time_point_sec>{"fc::time_point_sec"};
+template<> struct nullness<fc::time_point_sec> : pqxx::no_null<fc::time_point_sec> {};
+
+template<> struct string_traits<fc::time_point_sec>
+{
+  static constexpr bool converts_to_string{true};
+  static constexpr bool converts_from_string{true};
+
+private:
+  static constexpr std::tuple<uint32_t, unsigned, unsigned, unsigned, unsigned, unsigned> to_ymd_hms(uint32_t seconds_since_epoch) noexcept
+  {
+    uint32_t z = seconds_since_epoch / 86400;
+
+    z += 719468;
+    const uint32_t era = z / 146097;
+    const unsigned doe = static_cast<unsigned>(z - era * 146097);          // [0, 146096]
+    const unsigned yoe = (doe - doe/1460 + doe/36524 - doe/146096) / 365;  // [0, 399]
+    const uint32_t y = static_cast<uint32_t>(yoe) + era * 400;
+    const unsigned doy = doe - (365*yoe + yoe/4 - yoe/100);                // [0, 365]
+    const unsigned mp = (5*doy + 2)/153;                                   // [0, 11]
+    const unsigned d = doy - (153*mp+2)/5 + 1;                             // [1, 31]
+    const unsigned m = mp + (mp < 10 ? 3 : -9);                            // [1, 12]
+
+    const uint32_t seconds_since_day = seconds_since_epoch % 86400;
+    const uint32_t hours = seconds_since_day / 3600;
+    const uint32_t seconds_since_hour = seconds_since_day % 3600;
+    const uint32_t minutes = seconds_since_hour / 60;
+    const uint32_t seconds = seconds_since_hour % 60;
+
+    return std::make_tuple(y + (m <= 2), m, d, hours, minutes, seconds);
+  }
+
+  static constexpr void write_two_digits(char* s, uint32_t n)
+  {
+    const char* const digits = "00010203040506070809"
+                               "10111213141516171819"
+                               "20212223242526272829"
+                               "30313233343536373839"
+                               "40414243444546474849"
+                               "50515253545556575859"
+                               "60616263646566676869"
+                               "70717273747576777879"
+                               "80818283848586878889"
+                               "90919293949596979899" + 2 * n;
+    s[0] = digits[0];
+    s[1] = digits[1];
+  }
+public:
+
+  static zview to_buf(char *begin, char *end, const fc::time_point_sec& value)
+  {
+    // we can't optimize this any more, just call into_buf()
+    return generic_to_buf(begin, end, value);
+  }
+
+  static char *into_buf(char *begin, char *end, const fc::time_point_sec& value)
+  {
+    if (end - begin < 20)
+      throw conversion_overrun{"Could not write fc::time_point_sec: buffer too small."};
+    const auto [year, month, day, hour, minute, second] = to_ymd_hms(value.sec_since_epoch());
+
+    write_two_digits(begin, year / 100);
+    write_two_digits(begin + 2, year % 100);
+    begin[4] = '-';
+    write_two_digits(begin + 5, month);
+    begin[7] = '-';
+    write_two_digits(begin + 8, day);
+    begin[10] = 'T';
+    write_two_digits(begin + 11, hour);
+    begin[13] = ':';
+    write_two_digits(begin + 14, minute);
+    begin[16] = ':';
+    write_two_digits(begin + 17, second);
+    begin[19] = 0;
+    return begin + 20;
+  }
+
+  // we don't have to deal with years > 9999
+  static constexpr std::size_t size_buffer(const fc::time_point_sec &) noexcept { return 20; }
+
+  static fc::time_point_sec from_string(std::string_view text)
+  {
+    // note: not particularly fast, but haf doesn't currently use this at all
+    return fc::time_point_sec::from_iso_string(std::string(text));
+  }
+};
+
+// operation to BYTEA (the code would be about the same for any binary serialized object)
+template<> std::string const type_name<hive::protocol::operation>{"hive::protocol::operation"};
+template<> struct nullness<hive::protocol::operation> : pqxx::no_null<hive::protocol::operation> {};
+template<> struct string_traits<hive::protocol::operation>
+{
+  static constexpr bool converts_to_string{true};
+  static constexpr bool converts_from_string{true};
+  
+  static zview to_buf(char *begin, char *end, const fc::time_point_sec& value)
+  {
+    // we can't optimize this any more, just call into_buf()
+    return generic_to_buf(begin, end, value);
+  }
+  
+  static char *into_buf(char *begin, char *end, const hive::protocol::operation& value)
+  {
+    fc::datastream<char*> datastream(begin, end - begin);
+    try
+    {
+      fc::raw::pack(datastream, value);
+    }
+    catch (const fc::out_of_range_exception&)
+    {
+      throw conversion_overrun{"Could not write hive::protocol::operation: buffer too small."};
+    }
+    
+    // we know that serializing an operation will never lead to 0 bytes.
+    const size_t raw_size = datastream.tellp();
+    
+    if (end - begin < (long)size_buffer(value))
+      throw conversion_overrun{"Could not write hive::protocol::operation: buffer too small."};
+    
+    // so at this point, we have a buffer with the binary data at the beginning
+    // and then a bunch of uninitialized data after it.
+    // now hex-encode the string, working backwards from the end
+    char* end_of_data = begin + raw_size * 2 + 3;
+    char* null_terminator = end_of_data - 1;
+    *null_terminator = 0;
+    
+    // start raw_pointer pointing at the last byte of raw data
+    char* raw_pointer = begin + raw_size - 1;
+    // and the cooked_pointer at the destination of the last hex char
+    char* cooked_pointer = null_terminator - 1;
+    
+    for (size_t i = 0; i < raw_size; ++i)
+    {
+      unsigned char as_unsigned = *raw_pointer--;
+      *cooked_pointer-- = hex_digit(as_unsigned & 0x0f);
+      *cooked_pointer-- = hex_digit(as_unsigned >> 4);
+    }
+
+    // we should now have everything encoded, and just need to add the \x
+    *raw_pointer-- = 'x';
+    *raw_pointer-- = '\\';
+    
+    return end_of_data;
+  }
+  
+  static std::size_t size_buffer(const hive::protocol::operation& value)
+  {
+    fc::datastream<size_t> size_packer;
+    fc::raw::pack(size_packer, value);
+    return size_esc_bin(size_packer.tellp());
+  }
+
+  static hive::protocol::operation from_string(std::string_view text)
+  {
+    // TODO: dehexify `text`, this probably won't work as is
+    fc::datastream<const char*> datastream(text.data(), text.size());
+    hive::protocol::operation op;
+    fc::raw::unpack(datastream, op);
+    return op;
+  }
+};
+
+} // end namespace pqxx
 
 namespace hive::plugins::sql_serializer {
   /**
@@ -165,7 +448,7 @@ namespace hive::plugins::sql_serializer {
     FC_ASSERT(!data.empty(), "Data empty 2");
 
     tx.run_in_transaction([&](pqxx::work& work) {
-      pqxx::stream_to stream{work, TABLE_NAME, std::vector<std::string>{COLUMN_ARRAY, COLUMN_ARRAY + COLUMN_ARRAY_LEN}};
+      pqxx::stream_to stream = pqxx::stream_to::raw_table(work, TABLE_NAME, COLUMN_LIST);
       for (auto i = data.cbegin(); i != data.cend(); ++i) 
         stream << to_tuple(*i);
       stream.complete();
