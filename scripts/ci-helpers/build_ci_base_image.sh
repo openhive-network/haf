7ed4bcd97211af62f283598551eb77088dc79bbe
Bartek Wrona <wrona@syncad.com>
2023-12-17 16:28:01 +0100

    Unified values of CI_BASE_IMAGE and BUILDER_IMAGE docker args to avoid errors while building ci-base-image.

--- scripts/ci-helpers/build_ci_base_image.sh
+++ scripts/ci-helpers/build_ci_base_image.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 REGISTRY=${1:-registry.gitlab.syncad.com/hive/haf/}
-CI_IMAGE_TAG=:ubuntu22.04-7
+CI_IMAGE_TAG=ubuntu22.04-7
 
 # exit when any command fails
 set -e
 
 docker buildx build --progress=plain --target=ci-base-image \
   --build-arg CI_REGISTRY_IMAGE="$REGISTRY" --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
-  -t "${REGISTRY}ci-base-image$CI_IMAGE_TAG" -f Dockerfile .
+  --build-arg BUILD_IMAGE_TAG=$CI_IMAGE_TAG \
+  -t "${REGISTRY}ci-base-image:$CI_IMAGE_TAG" -f Dockerfile .
