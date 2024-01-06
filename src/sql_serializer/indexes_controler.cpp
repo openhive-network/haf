1eb3bac1ec3c5d7617ee915e732af56c4c7e0754
Dan Notestein <dan@syncad.com>
2024-01-06 07:13:55 +0000

    fix cluster msg sometimes printing erroneously

--- src/sql_serializer/indexes_controler.cpp
+++ src/sql_serializer/indexes_controler.cpp
@@ -22,13 +22,13 @@ indexes_controler::disable_indexes_depends_on_blocks( uint32_t number_of_blocks_
     return;
 
   bool disable_indexes = number_of_blocks_to_insert > _psql_index_threshold;
-  if (!_cluster_tables)
+  if (disable_indexes)
   {
-    ilog("we're dropping indexes, so schedule to recluster table(s)");
-    _cluster_tables = disable_indexes;
+    ilog("We are dropping indexes, so schedule to recluster table(s)");
+    _cluster_tables = true;
   }
-
-  if ( !disable_indexes ) {
+  else
+  {
     ilog( "Number of blocks to add is less than threshold for disabling indexes. Indexes won't be disabled. ${n}<${t}",("n", number_of_blocks_to_insert )("t", _psql_index_threshold ) );
     return;
   }
