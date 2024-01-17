88adfe04e995163d5a48e98de1392253969866e4
mkrzeminski <mkrzeminski@syncad.com>
2024-01-17 16:38:24 +0100

    formatting

--- src/hive_fork_manager/shared_lib/spixx.hpp
+++ src/hive_fork_manager/shared_lib/spixx.hpp
@@ -5,12 +5,12 @@
 typedef uintptr_t Datum;
 struct SPITupleTable;
 
-extern "C" 
+extern "C"
 {
   struct TupleDescData;
   typedef TupleDescData* TupleDesc;
   struct HeapTupleData;
-  typedef HeapTupleData *HeapTuple;
+  typedef HeapTupleData* HeapTuple;
   int SPI_connect();
   int SPI_finish();
 } // extern "C"
@@ -20,55 +20,51 @@ extern "C"
 namespace spixx
 {
 
-class field
+struct field
 {
-public:
-    Datum datum;
-public:
-    bool isNull;
+  Datum datum;
+  bool isNull;
 
-public:
-    [[nodiscard]] bool is_null() const noexcept;
+  [[nodiscard]] bool is_null() const noexcept;
+  [[nodiscard]] const char* c_str() const;
 
-    [[nodiscard]] const char *c_str() const;
-
-public:
-    uint32_t as_uint32_t() const;
-    int as_int() const;
-    int64_t as_int64_t() const;
-    std::string as_hex_string() const;
-    std::string as_timestamp_string() const ;
+  [[nodiscard]] uint32_t as_uint32_t() const;
+  [[nodiscard]] int as_int() const;
+  [[nodiscard]] int64_t as_int64_t() const;
+  [[nodiscard]] std::string as_hex_string() const;
+  [[nodiscard]] std::string as_timestamp_string() const;
 
 private:
-    size_t bytea_length() const;
+  [[nodiscard]] size_t bytea_length() const;
 };
-
 
 class binarystring
 {
 private:
-    const field& fld;
+  const field& fld;
 
 public:
-    explicit binarystring(const field&);
+  explicit binarystring(const field&);
 
-    using char_type = unsigned char;
-    using value_type = std::char_traits<char_type>::char_type;
-    using size_type = std::size_t;
-    [[nodiscard]] value_type const *data() const noexcept;
-    [[nodiscard]] size_type size() const noexcept;
+  using char_type = unsigned char;
+  using value_type = std::char_traits<char_type>::char_type;
+  using size_type = std::size_t;
+  [[nodiscard]] value_type const* data() const noexcept;
+  [[nodiscard]] size_type size() const noexcept;
 };
 
-class row {
+class row
+{
 protected:
-    HeapTuple tuple;
+  HeapTuple tuple;
+
 private:
-    TupleDesc tupdesc;
+  TupleDesc tupdesc;
 
 public:
-    row(HeapTuple t, TupleDesc td);
-    field operator[](const std::string& key) const;
-    std::string get_value(const std::string& key) const;
+  row(HeapTuple t, TupleDesc td);
+  field operator[](const std::string& key) const;
+  std::string get_value(const std::string& key) const;
 };
 
 // The iterator is also the row - it mimics the pqxx behavior where
@@ -76,36 +72,37 @@ public:
 class const_result_iterator : public row
 {
 private:
-    SPITupleTable *tuptable;
-    int index;
+  SPITupleTable* tuptable;
+  int index;
 
 public:
-    const_result_iterator(SPITupleTable *tt, int idx);
-    const_result_iterator();
+  explicit const_result_iterator(SPITupleTable* tt, int idx);
+  const_result_iterator();
 
-    const_result_iterator& operator++();
+  const_result_iterator& operator++();
 
-    [[nodiscard]] bool operator!=(const_result_iterator const& i) const;
-    [[nodiscard]] bool operator==(const_result_iterator const& i) const;
-    row operator*() const;
+  [[nodiscard]] bool operator!=(const_result_iterator const& i) const;
+  [[nodiscard]] bool operator==(const_result_iterator const& i) const;
+  row operator*() const;
 };
 
-class result {
+class result
+{
 private:
-    SPITupleTable *tuptable;
-    uint64_t proc;
+  SPITupleTable* tuptable;
+  uint64_t proc;
 
 public:
-    result();
-    result(SPITupleTable *t, TupleDesc td, uint64_t p);
+  result();
+  result(SPITupleTable* t, TupleDesc td, uint64_t p);
 
-    using const_iterator = const_result_iterator;
-    [[nodiscard]] const_iterator end() const noexcept;
-    [[nodiscard]] bool empty() const noexcept;
-    [[nodiscard]] const_iterator begin() const noexcept;
-    row operator[](size_t i) const noexcept;
+  using const_iterator = const_result_iterator;
+  [[nodiscard]] const_iterator end() const noexcept;
+  [[nodiscard]] bool empty() const noexcept;
+  [[nodiscard]] const_iterator begin() const noexcept;
+  row operator[](size_t i) const noexcept;
 
-    void display_column_names_and_types(const std::string& label) const;
+  void display_column_names_and_types(const std::string& label) const;
 };
 
 result execute_query(const std::string& query);
