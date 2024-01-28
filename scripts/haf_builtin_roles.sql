b2065d62d2b5205c5ec517feb5a24a46f4b9a36f
Bartek Wrona <wrona@syncad.com>
2024-01-28 14:39:35 +0100

    Eliminated haf_app_admin role creation

--- scripts/haf_builtin_roles.sql
+++ scripts/haf_builtin_roles.sql
@@ -28,21 +28,6 @@ $$;
 
 DO $$
 BEGIN
-    CREATE ROLE haf_app_admin WITH LOGIN CREATEROLE INHERIT IN ROLE hive_applications_group;
-    EXCEPTION WHEN DUPLICATE_OBJECT THEN
-    RAISE NOTICE 'haf_app_admin role already exists';
-END
-$$;
-
-DO $$
-BEGIN
-    -- This is needed in postgres 15 or later.  In 14, roles automatically have the admin option
-    GRANT hive_applications_group TO haf_app_admin WITH ADMIN OPTION;
-END
-$$;
-
-DO $$
-BEGIN
     CREATE ROLE hived WITH LOGIN INHERIT IN ROLE hived_group;
     EXCEPTION WHEN DUPLICATE_OBJECT THEN
     RAISE NOTICE 'hived role already exists';
