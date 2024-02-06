4f854c3a45044ccdc198a26bd7eefdd9f8c78a46
mkrzeminski <mkrzeminski@syncad.com>
2024-02-06 09:24:41 +0100

    pxx header cleanuop, comments

--- src/hive_fork_manager/shared_lib/pxx.hpp
+++ src/hive_fork_manager/shared_lib/pxx.hpp
@@ -6,6 +6,12 @@
 
 #define PQXX_PURE
 
+// This is the common interface for pqxx and SPI wrappeer (spixx)
+// it can be used with value semantics, not pointer or reference semantics
+// according to Sean Parent's 
+// Value Semantics and Concept-based Polymorphism
+// It has some drawbacks thou e.g.:  it is hard to expose the templated member interface  
+
 namespace pxx
 {
 
@@ -39,7 +45,6 @@ inline std::ostream& operator<<(std::ostream& os, const jsonb_string& timestamp)
     return os << timestamp.val;
 }
 
-
 using result_size_type = int;
 using row_size_type = int;
 
@@ -50,6 +55,8 @@ struct field_concept
   virtual char const *c_str() const & = 0;
   virtual bool is_null() const noexcept = 0;
 
+  // Cannot expose as<T> directly because C++ does not allow templated virtuals
+  // so casting to std::any
   virtual std::any as(const std::type_info& type) const = 0;
   
 };
@@ -73,7 +80,10 @@ struct field_model : field_concept
   {
       using operation_bin_type = std::basic_string<std::byte>;
 
-      if (type == typeid(uint32_t)) //mtlk todo into case switch
+      // Cannot use switch case with type_info
+      // and the visitor/variant does not bring any advantage here
+      // so if else used
+      if (type == typeid(uint32_t)) 
       {
           return data.template as<uint32_t>();
       }
@@ -105,11 +115,6 @@ struct field_model : field_concept
       {
         return data. template as<pxx::timestamp_wo_tz_type>();
       }
-    //   else if(type == typeid(std::string))
-    //   {
-    //     return data. template as<std::string>();
-    //   }
-      // Add other types as needed
       throw std::bad_cast();
   }
 
@@ -138,119 +143,111 @@ public:
 struct row;
 struct row_concept
 {
-  
-
-  //[[nodiscard]] reference operator[](zview col_name) const;
   virtual field operator[](const std::string& col_name) const = 0;
-  //[[nodiscard]] reference operator[](const char [4]) const;
-
 };
 
-
-// Model for row
 template<typename T>
-struct row_model : row_concept {
+struct row_model : row_concept 
+{
     T data;
     row_model(T data) : data(std::move(data)) {}
 
-    pxx::field operator[](const std::string& key) const override {
+    pxx::field operator[](const std::string& key) const override 
+    {
         return data[key];
     }
 };
 
 
 // Type-erased wrapper for row
-class row {
+class row 
+{
     std::unique_ptr<const row_concept> self_;
 public:
     template<typename T>
     row(T x) : self_(std::make_unique<row_model<T>>(std::move(x))) {}
 
-    pxx::field operator[](const std::string& key) const {
+    pxx::field operator[](const std::string& key) const 
+    {
         return (*self_)[key];
     }
 
 };
 
 
-struct const_result_iterator_concept {
-    virtual ~const_result_iterator_concept() = default;
-
-    virtual const_result_iterator_concept& operator++() = 0;
-    virtual bool operator!=(const const_result_iterator_concept& other) const = 0;
-    virtual bool operator==(const const_result_iterator_concept& other) const = 0;
-    virtual row  operator*() const = 0;
-
+struct const_result_iterator_concept
+{
+  virtual ~const_result_iterator_concept() = default;
+  virtual const_result_iterator_concept& operator++() = 0;
+  virtual row  operator*() const = 0;
   virtual bool is_equal(const const_result_iterator_concept& other) const = 0;
   virtual bool is_not_equal(const const_result_iterator_concept& other) const = 0;
-
 };
-
-
 
 
 template<typename T>
-struct const_result_iterator_model : const_result_iterator_concept {
-    T data;
-    const_result_iterator_model(T data) : data(std::move(data)) {}
+struct const_result_iterator_model : const_result_iterator_concept
+{
+  T data;
+  const_result_iterator_model(T data) : data(std::move(data)) {}
 
-    const_result_iterator_concept& operator++() override {
-        ++data;
-        return *this;
+  const_result_iterator_concept& operator++() override
+  {
+    ++data;
+    return *this;
+  }
+
+  bool is_equal(const const_result_iterator_concept& other) const override
+  {
+    const auto* other_ptr = static_cast<const const_result_iterator_model<T>*>(&other);
+    if(other_ptr) 
+    {
+      return data == other_ptr->data;
     }
+    return false;
+  }
 
-    bool operator!=(const const_result_iterator_concept& other) const override {
-        //return data.operator!=(other);
-        return true;
+  bool is_not_equal(const const_result_iterator_concept& other) const override
+  {
+    const auto* other_ptr = static_cast<const const_result_iterator_model<T>*>(&other);
+    if(other_ptr) 
+    {
+      return data != other_ptr->data;
     }
+    return true;
+  }
 
-    bool operator==(const const_result_iterator_concept& other) const override {
-        //return data.operator==(other);
-        return true;
-    }
-  bool is_equal(const const_result_iterator_concept& other) const override {
-        const auto* other_ptr = dynamic_cast<const const_result_iterator_model<T>*>(&other);
-        if (other_ptr) {
-            return data == other_ptr->data;
-        }
-        return false;
-    }
-
-    bool is_not_equal(const const_result_iterator_concept& other) const override {
-        const auto* other_ptr = dynamic_cast<const const_result_iterator_model<T>*>(&other);
-        if (other_ptr) {
-            return data != other_ptr->data;
-        }
-        return true;
-    }    
-
-    row  operator*() const override { return *data; } 
+  row  operator*() const override { return *data; }
 };
 
 // Type-erased wrapper for const_result_iterator
-class const_result_iterator {
-    std::unique_ptr<const_result_iterator_concept> self_;
+class const_result_iterator 
+{
+  std::unique_ptr<const_result_iterator_concept> self_;
 public:
-    template<typename T>
-    const_result_iterator(T x) : self_(std::make_unique<const_result_iterator_model<T>>(std::move(x))) {}
-    const_result_iterator(){} //mtlk todo ??
+  template<typename T>
+  const_result_iterator(T x) : self_(std::make_unique<const_result_iterator_model<T>>(std::move(x))) {}
+  const_result_iterator() {}
 
 
-    const_result_iterator& operator++() {
-        ++(*self_);
-        return *this;
-    }
+  const_result_iterator& operator++()
+  {
+    ++(*self_);
+    return *this;
+  }
 
-    bool operator==(const const_result_iterator& other) const {
-        return self_->is_equal(*other.self_);
-    }
+  bool operator==(const const_result_iterator& other) const
+  {
+    return self_->is_equal(*other.self_);
+  }
 
-    bool operator!=(const const_result_iterator& other) const {
-        return self_->is_not_equal(*other.self_);
-    }
+  bool operator!=(const const_result_iterator& other) const
+  {
+    return self_->is_not_equal(*other.self_);
+  }
 
 
-  row operator*() const { return **self_; } 
+  row operator*() const { return **self_; }
 };
 
 
@@ -267,19 +264,23 @@ struct result_concept
 
 // Model for pxx::result
 template<typename T>
-struct result_model : result_concept {
+struct result_model : result_concept 
+{
     T data;
     result_model(T data) : data(std::move(data)) {}
 
-    const_result_iterator begin() const noexcept override {
+    const_result_iterator begin() const noexcept override 
+    {
         return data.begin();
     }
 
-    const_result_iterator end() const noexcept override {
+    const_result_iterator end() const noexcept override 
+    {
         return data.end();
     }
 
-    bool empty() const noexcept override {
+    bool empty() const noexcept override 
+    {
         return data.empty();
     }
 
@@ -288,41 +289,31 @@ struct result_model : result_concept {
 };
 
 // Type-erased wrapper for pxx::result
-class result {
+class result 
+{
     std::unique_ptr<const result_concept> self_;
 public:
     template<typename T>
     result(T x) : self_(std::make_unique<result_model<T>>(std::move(x))) {}
     result(){ } //mtlk todo ??
 
-    const_result_iterator begin() const noexcept {
+    const_result_iterator begin() const noexcept 
+    {
         return self_->begin();
     }
 
-    const_result_iterator end() const noexcept {
+    const_result_iterator end() const noexcept 
+    {
         return self_->end();
     }
 
-    bool empty() const noexcept {
+    bool empty() const noexcept 
+    {
         return self_->empty();
     }
 
-
-
 };
 
-// struct result
-// {
-//   using size_type = result_size_type;
-//   using const_iterator = const_result_iterator;
-
-//   [[nodiscard]] row operator[](size_type i) const noexcept;
-//   [[nodiscard]] const_iterator begin() const noexcept;
-//   [[nodiscard]] inline const_iterator end() const noexcept;
-//   [[nodiscard]] PQXX_PURE bool empty() const noexcept;
 
 
-// };
-
-
-}  // namespace spixx
+}  // namespace pxx
