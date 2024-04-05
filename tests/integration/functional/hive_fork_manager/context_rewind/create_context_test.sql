5604da4b964642d4843cb41c47b400bc90b218e8
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-04-05 09:52:09 +0200

    more tests

--- tests/integration/functional/hive_fork_manager/context_rewind/create_context_test.sql
+++ tests/integration/functional/hive_fork_manager/context_rewind/create_context_test.sql
@@ -5,6 +5,9 @@ $BODY$
 BEGIN
     PERFORM hive.context_create( 'my_context' );
     PERFORM hive.context_create( 'my_context2' );
+
+    CREATE SCHEMA myapp;
+    PERFORM hive.context_create( _name => 'my_contextapp', _schema => 'myapp' );
 END
 $BODY$
 ;
@@ -16,6 +19,9 @@ $BODY$
 BEGIN
     ASSERT EXISTS ( SELECT FROM hive.contexts WHERE name = 'my_context' AND current_block_num = 0 AND irreversible_block = 0 AND is_attached = TRUE );
     ASSERT EXISTS ( SELECT FROM hive.contexts WHERE name = 'my_context2' AND current_block_num = 0 AND irreversible_block = 0 AND is_attached = TRUE );
+    ASSERT EXISTS ( SELECT FROM hive.contexts WHERE name = 'my_contextapp' AND current_block_num = 0 AND irreversible_block = 0 AND is_attached = TRUE );
+
+    ASSERT EXISTS ( SELECT FROM information_schema.tables WHERE table_schema='myapp' AND table_name  = 'my_contextapp' ), 'Context table in schema myapp does not exist';
 END
 $BODY$
 ;
