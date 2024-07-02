68ab1958b36a5dfa2dd2487cccf3c8f4dad02f3c
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-07-02 08:00:32 +0200

    better test commits

--- tests/integration/functional/hive_fork_manager/application_loop/loop_commits.sql
+++ tests/integration/functional/hive_fork_manager/application_loop/loop_commits.sql
@@ -1,3 +1,24 @@
+CREATE OR REPLACE PROCEDURE hived_test_given()
+    LANGUAGE 'plpgsql'
+AS
+$BODY$
+BEGIN
+    INSERT INTO hive.blocks
+    VALUES
+           ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+         , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:22-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+         , ( 3, '\xBADD30', '\xCAFE30', '2016-06-22 19:10:23-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+         , ( 4, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+         , ( 5, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
+    ;
+
+    INSERT INTO hive.accounts( id, name, block_num )
+    VALUES (5, 'initminer', 1)
+    ;
+END;
+$BODY$;
+
+
 CREATE OR REPLACE PROCEDURE alice_test_given()
     LANGUAGE 'plpgsql'
 AS
@@ -11,6 +32,8 @@ DECLARE
 BEGIN
     CREATE SCHEMA alice;
     PERFORM hive.app_create_context( 'alice', 'alice', _stages => __context_stages );
+
+    CREATE TABLE alice_table( value INTEGER );
 END;
 $BODY$;
 
@@ -30,5 +53,11 @@ BEGIN
     __tx_id_before_next_id :=  txid_current();
     CALL hive.app_next_iteration( 'alice', __range_placeholder );
     ASSERT txid_current() != __tx_id_before_next_id, 'previous tx not closed';
+
+    __tx_id_before_next_id :=  txid_current();
+    PERFORM * FROM hive.blocks;
+    INSERT INTO alice_table VALUES (10),(11),(12);
+    CALL hive.app_next_iteration( 'alice', __range_placeholder );
+    ASSERT txid_current() != __tx_id_before_next_id, 'previous tx not closed(2)';
 END;
 $BODY$;
