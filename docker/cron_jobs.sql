45326a93e912b40558db15788b4f5f682de9eb41
Krzysztof Leśniak <klesniak@syncad.com>
2024-09-03 16:30:31 +0200

    Add cron jobs

---
+++ docker/cron_jobs.sql
@@ -0,0 +1,3 @@
+SET ROLE haf_maintainer;
+SELECT cron.schedule('dead_app_contexts_auto_detach', '0 */4 * * *', $$CALL hive.proc_perform_dead_app_contexts_auto_detach()$$);
+SELECT cron.schedule('delete-job-run-details', '0 12 * * *', $$DELETE FROM cron.job_run_details WHERE end_time < now() - INTERVAL '7 DAYS'$$);
