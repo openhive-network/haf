3c98bcd2d64a97cec4a1803c6fe16d5dbbbfc78e
Krzysztof Leśniak <klesniak@syncad.com>
2024-09-10 12:13:38 +0200

    Quote postgres access var

--- tests/integration/system/applications/auto_detaching/scenario1.sh
+++ tests/integration/system/applications/auto_detaching/scenario1.sh
@@ -16,17 +16,17 @@ POSTRGRES_ADMIN_URL="postgresql://haf_admin@${POSTGRES_HOST}/haf_block_log"
 
 POSTGRES_ARGS="-aw -v ON_ERROR_STOP=ON"
 
-psql ${POSTRGRES_ADMIN_URL} ${POSTGRES_ARGS} -f "${SCRIPTPATH}/test_app.sql"
-psql ${POSTRGRES_ADMIN_URL} ${POSTGRES_ARGS} -f "${SCRIPTPATH}/test_utils.sql"
-psql ${POSTRGRES_APP_URL} ${POSTGRES_ARGS} -f "${SCRIPTPATH}/scenario1.sql"
+psql "${POSTRGRES_ADMIN_URL}" ${POSTGRES_ARGS} -f "${SCRIPTPATH}/test_app.sql"
+psql "${POSTRGRES_ADMIN_URL}" ${POSTGRES_ARGS} -f "${SCRIPTPATH}/test_utils.sql"
+psql "${POSTRGRES_APP_URL}" ${POSTGRES_ARGS} -f "${SCRIPTPATH}/scenario1.sql"
 
 # Now execute test
 
 TIMESHIFT="'3 hrs'::interval"
 
-psql ${POSTRGRES_APP_URL} ${POSTGRES_ARGS} -c "CALL test.scenario1_prepare(${TIMESHIFT});"
+psql "${POSTRGRES_APP_URL}" ${POSTGRES_ARGS} -c "CALL test.scenario1_prepare(${TIMESHIFT});"
 
-psql ${POSTRGRES_ADMIN_URL} ${POSTGRES_ARGS} -c 'SET ROLE haf_maintainer;' -c "CALL hive.proc_perform_dead_app_contexts_auto_detach(${TIMESHIFT} - '1 min'::interval);"
+psql "${POSTRGRES_ADMIN_URL}" ${POSTGRES_ARGS} -c 'SET ROLE haf_maintainer;' -c "CALL hive.proc_perform_dead_app_contexts_auto_detach(${TIMESHIFT} - '1 min'::interval);"
 
-psql ${POSTRGRES_APP_URL} ${POSTGRES_ARGS} -c "SET ROLE test_app_owner; CALL test.scenario1_verify(${TIMESHIFT});"
+psql "${POSTRGRES_APP_URL}" ${POSTGRES_ARGS} -c "SET ROLE test_app_owner; CALL test.scenario1_verify(${TIMESHIFT});"
 
