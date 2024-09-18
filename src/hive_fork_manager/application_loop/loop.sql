44a6a6fb93fdc2f5fe917400901470257417c7b7
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-09-18 11:57:04 +0200

    Revert "remove pessimistic lock from loop.sql"
    This reverts commit 4c2e9ecdc45467fc0c6f175055770521867f0917.

--- src/hive_fork_manager/application_loop/loop.sql
+++ src/hive_fork_manager/application_loop/loop.sql
@@ -167,6 +167,11 @@ BEGIN
     -- 2. find current stage if:
     IF hive.is_stages_analyze_required( __lead_context_state, hive.get_irreversible_head_block() )
     THEN
+        PERFORM  1
+        FROM hive.contexts c
+        JOIN hive.contexts_attachment hca ON hca.context_id = c.id
+        WHERE c.name = ANY(_contexts) FOR UPDATE;
+
         IF NOT hive.app_context_are_attached( _contexts )
         THEN
             PERFORM hive.app_context_attach( _contexts );
