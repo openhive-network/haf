bf2f68d5929f634d40a663f499619861074ba431
Bartek Wrona <wrona@syncad.com>
2024-10-23 17:28:33 +0200

    sleep_infinity.sh script uses ncat command to listen of configured HTTP_PORT (instead of python http server)

--- scripts/maintenance-scripts/sleep_infinity.sh
+++ scripts/maintenance-scripts/sleep_infinity.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-set -xeuo pipefail
+set -euo pipefail
+
+echo "You can now connect to the database.  This this container will continue to exist until you shut it down"
 
 # gitlab healthchecks are testing whether this port is open so we know when container started
-python3 -m http.server $HTTP_PORT
+while true; do echo -e "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n" | nc -l -p $HTTP_PORT; done
