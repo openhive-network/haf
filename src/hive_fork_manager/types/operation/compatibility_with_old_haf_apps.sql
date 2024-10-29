784c92bd4e4223220ec83b0d28e11b7cfce60b5c
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-10-29 11:06:59 +0100

    remove hive.operation domain
    While using the hive.operation domain for HAF clients was initially appealing, it proved impractical. To enable the PostgreSQL planner to utilize indexes on the hafd.operation table, explicit casts to hafd.operation were required, which ultimately broke encapsulation. This undermined the purpose of introducing hive.operation and nullified its intended benefits.

--- src/hive_fork_manager/types/operation/compatibility_with_old_haf_apps.sql
+++ src/hive_fork_manager/types/operation/compatibility_with_old_haf_apps.sql
@@ -1,6 +1,5 @@
 -- for compatibility with old HAF apps add some aliases for types moved to hafd schema
 
-CREATE DOMAIN hive.operation AS hafd.operation;
 CREATE DOMAIN hive.application_stages AS hafd.application_stage[];
 CREATE DOMAIN hive.context_name AS hafd.context_name;
 CREATE DOMAIN hive.contexts_group AS hafd.context_name[] NOT NULL CONSTRAINT non_empty_contexts_group CHECK( CARDINALITY( VALUE ) > 0 );
