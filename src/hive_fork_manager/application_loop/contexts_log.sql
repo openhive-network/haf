412e78fd8e446e9d4bcc443cd1fae074430b2863
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-11-07 14:18:13 +0100

    start function

--- src/hive_fork_manager/application_loop/contexts_log.sql
+++ src/hive_fork_manager/application_loop/contexts_log.sql
@@ -9,19 +9,22 @@ CREATE TYPE hafd.context_event AS ENUM(
 );
 
 CREATE TABLE hafd.contexts_log(
-      context_id INTEGER
-    , application_state hafd.stage_name -- can be null
-    , event_type hafd.context_event
-    , date TIMESTAMP WITHOUT TIME ZONE
+      id BIGSERIAL NOT NULL
+    , context_name hafd.context_name NOT NULL
+    , application_state hafd.stage_name NOT NULL
+    , event_type hafd.context_event NOT NULL
+    , date TIMESTAMP WITHOUT TIME ZONE NOT NULL
     , application_block INTEGER NOT NULL
     , application_fork BIGINT NOT NULL
     , head_block  INTEGER NOT NULL
     , head_fork_id BIGINT NOT NULL
+    , CONSTRAINT pk_hive_contexts_log PRIMARY KEY( id )
 );
 
+SELECT pg_catalog.pg_extension_config_dump('hafd.contexts_log', '');
+SELECT pg_catalog.pg_extension_config_dump('hafd.contexts_log_id_seq', '');
 -- it is not possible to make FK t check if block/fork in the table row exists
 -- because reversible blocks are removed and a give pair og  block/fork may not exists any more
 -- similar situation with contexts, which can be removed
--- TODO(mickiewicz@syncad.com): what to do with entries when context is removed ?
---
+
 
