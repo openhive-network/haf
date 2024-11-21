98fb0356446ad2fe0e29e3b198a8f2f5e127dca1
Krzysztof Leśniak <klesniak@syncad.com>
2024-11-21 13:24:02 +0100

    Add id, status and is_app_defined columns to hafd.indexes_constraints

--- src/hive_fork_manager/hived_api_impl_indexes.sql
+++ src/hive_fork_manager/hived_api_impl_indexes.sql
@@ -1,7 +1,11 @@
+CREATE TYPE hafd.index_status AS ENUM ('missing', 'creating', 'created');
 CREATE TABLE IF NOT EXISTS hafd.indexes_constraints (
+    id SERIAL PRIMARY KEY,
     table_name text NOT NULL,
     index_constraint_name text NOT NULL,
     command text NOT NULL,
+    status text NOT NULL DEFAULT 'missing',
+    is_app_defined BOOLEAN NOT NULL DEFAULT FALSE,
     is_constraint boolean NOT NULL,
     is_index boolean NOT NULL,
     is_foreign_key boolean NOT NULL,
