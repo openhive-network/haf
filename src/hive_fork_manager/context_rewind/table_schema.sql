d3de8c8165c0a86cb0f2a268a4be72d8fd4b3760
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-12-03 13:41:47 +0100

    state providers hashes considered during update procedure
    Hash of start_provider functions bodies is computed and included into whole database hash.
    It means that when the definitions of state providers tables are changed, then update is not possible.

--- src/hive_fork_manager/context_rewind/table_schema.sql
+++ src/hive_fork_manager/context_rewind/table_schema.sql
@@ -1,17 +1,14 @@
-
-CREATE TABLE IF NOT EXISTS hafd.verify_table_schema(
-    table_name TEXT NOT NULL,
-    table_schema TEXT NOT NULL,
+CREATE TYPE hafd.verify_table_schema AS(
+    table_name TEXT,
+    table_schema TEXT,
     table_schema_hash UUID,
     columns_hash UUID,
     constraints_hash UUID,
     indexes_hash UUID,
-    table_columns TEXT NOT NULL,
-    table_constraints TEXT NOT NULL,
-    table_indexes TEXT NOT NULL
+    table_columns TEXT,
+    table_constraints TEXT,
+    table_indexes TEXT
 );
-
-SELECT pg_catalog.pg_extension_config_dump('hafd.verify_table_schema', '');
 
 CREATE TABLE IF NOT EXISTS hafd.table_schema(
     schema_name TEXT NOT NULL,
@@ -19,4 +16,9 @@ CREATE TABLE IF NOT EXISTS hafd.table_schema(
 );
 
 SELECT pg_catalog.pg_extension_config_dump('hafd.table_schema', '');
+
+CREATE TYPE hafd.state_provider_and_hash AS(
+    provider hafd.state_providers,
+    hash TEXT
+);
 
