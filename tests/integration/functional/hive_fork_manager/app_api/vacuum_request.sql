1a0ef2722fb313bdf1f9b57e6e66c714ed417230
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-12-10 14:01:35 +0100

    add test for vacuum checker

---
+++ tests/integration/functional/hive_fork_manager/app_api/vacuum_request.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE PROCEDURE alice_test_given()
+    LANGUAGE 'plpgsql'
+AS
+$BODY$
+BEGIN
+    CREATE SCHEMA A;
+    PERFORM hive.app_create_context( _name =>  'context', _schema => 'a'  );
+    CREATE TABLE A.table1(id  INTEGER ) INHERITS( a.context );
+END;
+$BODY$
+;
+
+CREATE OR REPLACE PROCEDURE alice_impersonal_test_when()
+    LANGUAGE 'plpgsql'
+AS
+$BODY$
+BEGIN
+    PERFORM hive.request_table_vacuum('a.table1');
+END;
+$BODY$
+;
+
+CREATE OR REPLACE PROCEDURE alice_test_then()
+    LANGUAGE 'plpgsql'
+AS
+$BODY$
+BEGIN
+   ASSERT
+       (SELECT COUNT(*) FROM hafd.vacuum_requests WHERE table_name = 'a.table1' ) = 1
+        , 'a.table1 wrong number of requests';
+
+   ASSERT
+       (SELECT status FROM hafd.vacuum_requests WHERE table_name = 'a.table1' ) = 'requested'
+       , 'a.table1 status != requested';
+END;
+$BODY$
+;
