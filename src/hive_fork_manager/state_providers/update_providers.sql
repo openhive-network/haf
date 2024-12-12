00aeb18f08d1994af5f141b36beb1e8d8a9a22e5
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-12-12 15:56:42 +0100

    update state providers during updating haf

---
+++ src/hive_fork_manager/state_providers/update_providers.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION hive.state_provider_update_runtime( _provider hafd.state_providers, _context hafd.context_name)
+    RETURNS void
+    LANGUAGE plpgsql
+AS
+$BODY$
+BEGIN
+    EXECUTE format('hive.runtimecode_provider_%s(%L)', _provider, _context );
+END;
+$BODY$
+;
+
+CREATE OR REPLACE FUNCTION hive.state_providers_update_runtime( )
+    RETURNS void
+    LANGUAGE plpgsql
+AS
+$BODY$
+BEGIN
+    PERFORM
+        hive.state_provider_update_runtime(spr.state_provider, hc.name )
+    FROM hafd.state_providers_registered spr
+    JOIN hafd.contexts hc ON hc.id = spr.context_id;
+END;
+$BODY$
+;
+
+SELECT hive.state_providers_update_runtime();
+
