32a2dfb963c220030961cbb58a87480ce3d83c3d
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-01-07 13:59:16 +0100

    verify_table_schema in update sql file

--- src/hive_fork_manager/context_rewind/table_schema.sql
+++ src/hive_fork_manager/context_rewind/table_schema.sql
@@ -1,15 +1,3 @@
-CREATE TYPE hafd.verify_table_schema AS(
-    table_name TEXT,
-    table_schema TEXT,
-    table_schema_hash UUID,
-    columns_hash UUID,
-    constraints_hash UUID,
-    indexes_hash UUID,
-    table_columns TEXT,
-    table_constraints TEXT,
-    table_indexes TEXT
-);
-
 CREATE TABLE IF NOT EXISTS hafd.table_schema(
     schema_name TEXT NOT NULL,
     schema_hash UUID NOT NULL
