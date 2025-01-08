2478963a6ba0268c40fc2947a5824a546fba873e
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-01-08 11:37:20 +0100

    remove hafd.table_schema table
    After many changes hash computed on databse could only be used to
    check if  hfm can be updated to a given new version. The hash cannot be used
    to check if the database schema for a given hfm version was modified,
    becuase it does not take all haf elements for computation. It means
    there is no need to have stored hash of the databas because it has no
    usage, moreover it is misleading and could mask that the some parts of
     the schema was modified.
    
    Warning: the change modifies hafd schema, what means that old hfm
    versions cannot be updated to it.

--- src/hive_fork_manager/context_rewind/table_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS hafd.table_schema(
-    schema_name TEXT NOT NULL,
-    schema_hash UUID NOT NULL
-);
-
-SELECT pg_catalog.pg_extension_config_dump('hafd.table_schema', '');
-
-
-
