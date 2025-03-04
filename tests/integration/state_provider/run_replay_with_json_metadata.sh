bd2bc44953cff9594f557ac791af0e728201d408
Konrad Botor <kbotor@syncad.com>
2025-03-04 13:18:10 +0100

    Update 20

--- tests/integration/state_provider/run_replay_with_json_metadata.sh
+++ tests/integration/state_provider/run_replay_with_json_metadata.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 CURRENT_PROJECT_DIR="$CI_PROJECT_DIR/tests/integration/state_provider"
+# shellcheck source=./state_provider_common_run.sh
 source "${CURRENT_PROJECT_DIR}/state_provider_common_run.sh" metadata
