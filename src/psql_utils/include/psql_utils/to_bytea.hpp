6127e5364e0e81f72fb4f5aad3d96d42c2c4a927
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-03-26 12:28:50 +0100

    fix for gcc warning about mismatch between memory allocationd and its freeing

--- src/psql_utils/include/psql_utils/to_bytea.hpp
+++ src/psql_utils/include/psql_utils/to_bytea.hpp
@@ -5,10 +5,12 @@
 #include <memory>
 
 namespace PsqlTools::PsqlUtils {
+    using bytea_ptr = std::unique_ptr< bytea, decltype(&std::free) >;
 
     template< typename _Data >
-    std::unique_ptr< bytea > toBytea( _Data* _data ) {
-      std::unique_ptr< bytea > result( ( bytea* )new uint8_t[ sizeof( _Data ) + VARHDRSZ ] );
+    bytea_ptr toBytea( _Data* _data ) {
+
+      bytea_ptr result( static_cast<bytea*>( std::malloc(sizeof(_Data) + VARHDRSZ) ), &std::free );
       SET_VARSIZE( result.get(), sizeof( _Data ) );
       std::memcpy( VARDATA_ANY( result.get() ), _data, sizeof( _Data ) );
 
