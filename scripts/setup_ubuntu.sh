96514b7a732c2844bde4a857b991fd0f1f05a687
Eric Frias <efrias@syncad.com>
2025-04-06 18:17:50 +0000

    WIP build against postgresql 18-devel

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -84,12 +84,33 @@ install_all_dev_packages() {
           postgresql-common
 
   /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y
-  DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql-17 postgresql-server-dev-17 postgresql-17-cron \
-    netcat-openbsd \
-    git python3.12 python3.12-venv python3.12-dev python3-pip postgresql-17-pgvector postgresql-plpython3-17 curl # for hivesense
+/usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y
+
+  sed -i -e 's/Suites: noble-pgdg/Suites: noble-pgdg-snapshot/'  -e 's/Components: main/Components: main 18/' /etc/apt/sources.list.d/pgdg.sources
+  echo 'Package: *' > /etc/apt/preferences.d/pgdg.pref
+  echo 'Pin: origin apt.postgresql.org' >> /etc/apt/preferences.d/pgdg.pref
+  echo 'Pin-Priority: 1001' >> /etc/apt/preferences.d/pgdg.pref
+
+  apt-get update
+
+  DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql-18 postgresql-server-dev-18 \
+  netcat-openbsd \
+  git python3.12 python3.12-venv python3.12-dev python3-pip postgresql-18-pgvector postgresql-plpython3-18 curl # for hivesense
 
   apt-get clean
   rm -rf /var/lib/apt/lists/*
+
+  pushd /tmp
+  git clone https://github.com/citusdata/pg_cron.git
+  cd pg_cron
+
+  # Apply patch to make pg_cron compatible with PostgreSQL 18
+  sed -i 's/PortalDefineQuery(portal, NULL, sql, commandTag, plantree_list, NULL);/PortalDefineQuery(portal, NULL, sql, commandTag, plantree_list, NULL, NULL);/' src/pg_cron.c
+  make && make install
+  cd ..
+  rm -r pg_cron
+  popd
+
 
   sudo usermod -a -G users -c "PostgreSQL daemon account" postgres
 
