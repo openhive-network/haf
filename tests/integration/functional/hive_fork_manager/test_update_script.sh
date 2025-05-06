b198d38e26f832da0f5cc7d29fb8b433d19575b6
Krzysztof Leśniak <klesniak@syncad.com>
2025-05-06 09:35:51 +0200

    Change references to Postgres 17 in functional tests

--- tests/integration/functional/hive_fork_manager/test_update_script.sh
+++ tests/integration/functional/hive_fork_manager/test_update_script.sh
@@ -7,7 +7,7 @@ set -eu -o pipefail
 SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 SCRIPTS_DIR="$SCRIPTPATH/../../../../scripts"
 UPDATE_DB_NAME=update_db_test
-POSTGRES_VERSION=17
+POSTGRES_VERSION=18
 
 export PGUSER="haf_admin"
 export PGHOST="/var/run/postgresql"
