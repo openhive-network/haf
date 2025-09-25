a3664eea832cc98955d77b73d50ed6a2f6cbc4c6
Bartek Wrona <wrona@syncad.com>
2025-09-25 02:19:11 +0200

    Defined unit test specific to asset utility functions.

---
+++ tests/integration/functional/hive_fork_manager/shared_lib/verify_asset_utilities.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE PROCEDURE haf_admin_test_when()
+LANGUAGE 'plpgsql'
+    AS
+$BODY$
+DECLARE
+    hbdAssetSymbol hive.asset_symbol;
+    hiveAssetSymbol hive.asset_symbol;
+    vestsAssetSymbol hive.asset_symbol;
+    customAssetSymbol hive.asset_symbol;
+
+    assetInfo hive.asset_symbol_info;
+BEGIN
+    hiveAssetSymbol = SELECT hive.asset_symbol_from_nai_string('@@000000021', 3::smallint));
+    ASSERT(hiveAssetSymbol == 3200000035);
+
+    hbdAssetSymbol = SELECT hive.asset_symbol_from_nai_string('@@000000013', 3::smallint);
+    ASSERT(hbdAssetSymbol == 3200000003);
+
+    vestsAssetSymbol = SELECT hive.asset_symbol_from_nai_string('@@000000037', 6::smallint);
+    ASSERT(vestsAssetSymbol  == 3200000070);
+
+    ASSERT(SELECT hive.get_paired_symbol(hiveAssetSymbol) == vestsAssetSymbol);
+    ASSERT(SELECT hive.get_paired_symbol(vestsAssetSymbol) == hiveAssetSymbol);
+
+    customAssetSymbol = SELECT hive.asset_symbol_from_nai_string('@@999867365', 3::smallint);
+    ASSERT(customAssetSymbol == 3199575571);
+
+    assetInfo = SELECT hive.decode_asset_symbol(3199575571);
+    ASSERT(assetInfo == '(3,999867365,t,f)'::hive.asset_symbol_info);
+
+    --- Now get vesting version of custom asset
+    customAssetSymbol = SELECT hive.get_paired_symbol(3199575571);
+    ASSERT(customAssetSymbol == 3199575603);
+
+    assetInfo = SELECT hive.decode_asset_symbol(3199575603);
+    ASSERT(assetInfo == '(3,999867379,f,f)'::hive.asset_symbol_info);
+
+END;
+$BODY$
+;
+
+
+
+
