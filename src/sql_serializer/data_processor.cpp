0dd66f275dbb36b600adfda00c5a98151b2981fd
Krzysztof Leśniak <klesniak@syncad.com>
2025-10-28 13:03:36 +0100

    Kill node on query cancelled

--- src/sql_serializer/data_processor.cpp
+++ src/sql_serializer/data_processor.cpp
@@ -220,6 +220,7 @@ data_processor::handle_exception( std::exception_ptr exception_ptr ) {
     if (ex.sqlstate() == "57014") // query cancelled
     {
       wlog("Data processor ${d} detected SQL cancellation. Failing statement: `${q}'.", ("d", _description)("q", ex.query()));
+      kill_node();
     }
     else
     {
