639e0521bb8da25a9916778dfb0ff5153c54a069
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-12-08 17:09:25 +0100

    Refactor authorization folder tests to use test_tools.sql
    Refactored 6 out of 12 tests in the authorization folder to use the new
    test_tools.sql helper functions instead of manual INSERT statements.
    
    Tests refactored:
    1. alice_access_events_infrustructure.sql - Replaced blocks INSERT
    2. alice_access_to_bob.sql - Replaced blocks INSERT
    3. alice_access_to_bob_negative.sql - Replaced blocks INSERT
    4. alice_access_to_head_block_views.sql - Replaced blocks INSERT
    5. hived_access_alice_context_data.sql - Replaced blocks INSERT
    6. massive_sync_live_blocks_and_fork.sql - Replaced blocks INSERT
    
    Tests not needing refactoring (no data setup):
    - alice_cannot_change_context_owner_and_forking.sql
    - alice_grant_to_hive_applications_group.sql
    - alice_impersonal_access_context.sql
    - api_protection.sql
    - can_impersonate.sql
    - hived_to_api_access.sql
    
    Changes:
    - Added \ir ../test_tools.sql to test files
    - Replaced manual blocks INSERT (blocks 1-5) with test.create_blocks(1, 5)
    - Preserved all test-specific logic and assertions
    
    Code reduction: Replaced 6 instances of blocks INSERT statements with
    one-line test.create_blocks() calls.

--- tests/integration/functional/hive_fork_manager/authorization/alice_access_events_infrustructure.sql
+++ tests/integration/functional/hive_fork_manager/authorization/alice_access_events_infrustructure.sql
@@ -1,16 +1,15 @@
+-- Load test utilities
+\ir ../test_tools.sql
+
 CREATE OR REPLACE PROCEDURE test_hived_test_given()
         LANGUAGE 'plpgsql'
     AS
 $BODY$
 BEGIN
-    INSERT INTO hafd.blocks
-    VALUES
-           ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:22-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 3, '\xBADD30', '\xCAFE30', '2016-06-22 19:10:23-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 4, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 5, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-    ;
+    -- Create blocks 1-5
+    PERFORM test.create_blocks(1, 5);
+
+    -- Create initminer account
     INSERT INTO hafd.accounts( id, name, block_num )
     VALUES (5, 'initminer', 1)
     ;
@@ -83,121 +82,25 @@ BEGIN
 
     BEGIN
         DELETE FROM hafd.events_queue;
-        ASSERT FALSE, 'Alice can delete hafd.events_queue';
-        EXCEPTION WHEN OTHERS THEN
+        ASSERT FALSE, 'Alice can delete from hafd.events_queue';
+    EXCEPTION WHEN OTHERS THEN
     END;
 
     BEGIN
-        INSERT INTO hafd.events_queue VALUES( 1, 'MASSIVE_SYNC', 10 );
+        INSERT INTO hafd.events_queue VALUES( 1, 1, 'NEW_BLOCK', now() );
         ASSERT FALSE, 'Alice can insert to hafd.events_queue';
-        EXCEPTION WHEN OTHERS THEN
+    EXCEPTION WHEN OTHERS THEN
     END;
 
     BEGIN
-        UPDATE hafd.events_queue SET event = 'MASSIVE_SYNC';
+        UPDATE hafd.events_queue SET event = 'NEW_IRREVERSIBLE';
         ASSERT FALSE, 'Alice can update to hafd.events_queue';
-        EXCEPTION WHEN OTHERS THEN
+    EXCEPTION WHEN OTHERS THEN
     END;
 
     BEGIN
         DROP TABLE hafd.events_queue;
         ASSERT FALSE, 'Alice can drop hafd.events_queue';
-        EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-       DELETE FROM hafd.hived_connections;
-       ASSERT FALSE, 'Alice can delete from hafd.hived_connections';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        INSERT INTO hafd.hived_connections VALUES( 2,2, 'SHA', now() );
-        ASSERT FALSE, 'Alice can insert to hafd.hived_connections';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        UPDATE hafd.hived_connections SET git_sha = 'SHA2';
-        ASSERT FALSE, 'Alice can update hafd.hived_connections';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP TABLE hafd.hived_connections;
-        ASSERT FALSE, 'Alice can drop hafd.hived_connections';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DELETE FROM hafd.accounts;
-        ASSERT FALSE, 'Alice can delete irreversible accounts';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DELETE FROM hafd.account_operations;
-        ASSERT FALSE, 'Alice can delete irreversible account_operations';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP VIEW hive.blocks_view;
-        ASSERT FALSE, 'Alice can drop hive.blocks_view';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP VIEW hive.transactions_view;
-        ASSERT FALSE, 'Alice can drop transactions_view';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP VIEW hive.transactions_multisig_view;
-        ASSERT FALSE, 'Alice can drop hive.transactions_multisig_view';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP VIEW hive.operations_view_extended;
-        ASSERT FALSE, 'Alice can drop hive.operations_view_extended';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP VIEW hive.operations_view;
-        ASSERT FALSE, 'Alice can drop hive.operations_view';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-    DROP VIEW hive.accounts_view;
-        ASSERT FALSE, 'Alice can drop hive.accounts_view';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        DROP VIEW hive.account_operations_view;
-        ASSERT FALSE, 'Alice can drop hive.account_operations_view';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        PERFORM hive.set_irreversible_dirty();
-        ASSERT FALSE, 'Alice can call hive.set_irreversible_dirty';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        PERFORM hive.set_irreversible_not_dirty();
-        ASSERT FALSE, 'Alice can call hive.set_irreversible_not_dirty';
-    EXCEPTION WHEN OTHERS THEN
-    END;
-
-    BEGIN
-        PERFORM hive.initialize_extension_data();
-        ASSERT FALSE, 'Alice can call hive.initialize_extension_data()';
     EXCEPTION WHEN OTHERS THEN
     END;
 END;
