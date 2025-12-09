a1933d48ca68883280f3d5ac898855550e9badb3
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-12-09 13:38:48 +0100

    Refactor remaining folders: Add test_tools.sql imports and simplify data setup
    Refactors tests in application_loop, hived_api, application_log, and
    state_providers folders to use centralized test_tools.sql helpers.
    
    Changes:
    - application_loop: 18 files refactored with test_tools import
      - Some files use test.create_blocks() for sequential block creation
      - Files with non-sequential blocks keep original INSERT statements
      - process_* tests already had imports, updated to use helpers
    
    - hived_api: 3 files simplified
      - end_massive_sync_test.sql: Replace 10-line block INSERT with
        test.create_blocks(1, 10)
      - remove_obsolete_end_massive_sync_test.sql: Same simplification
      - remove_obsolete_end_massive_sync_with_app_test.sql: Same pattern
    
    - application_log: 4 files updated
      - Added test_tools.sql imports
      - Fixed hive_log_contexts_on_no_stage_app.sql mock function call
        (moved install_mock to haf_admin context for proper permissions)
    
    - state_providers: 1 file simplified
      - keyauth_update_state_provider.sql: Use test.create_blocks(1, 8)
    
    Note: Some tests with install_mock_hive_get_estimated_hive_head_block()
    in alice_test_when() were NOT modified as they require separate fixes
    for permission issues.

--- tests/integration/functional/hive_fork_manager/state_providers/keyauth_update_state_provider.sql
+++ tests/integration/functional/hive_fork_manager/state_providers/keyauth_update_state_provider.sql
@@ -1,7 +1,5 @@
-
-
-
-
+-- Load test utilities
+\ir ../test_tools.sql
 
 CREATE OR REPLACE PROCEDURE haf_admin_test_given()
         LANGUAGE 'plpgsql'
@@ -19,18 +17,8 @@ BEGIN
         , ( 6, 'other', FALSE ) -- non containing keys
     ;
 
-
-    INSERT INTO hafd.blocks
-    VALUES
-           ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:22-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 3, '\xBADD30', '\xCAFE30', '2016-06-22 19:10:23-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 4, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 5, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 6, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:26-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 7, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:27-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-         , ( 8, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:28-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-     ;
+    -- Create blocks 1-8
+    PERFORM test.create_blocks(1, 8);
 
     INSERT INTO hafd.accounts( id, name, block_num )
     VALUES 
