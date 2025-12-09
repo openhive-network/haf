36aec16ef21705c39e64b7dd1707cccfd796666b
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-12-09 12:52:28 +0100

    Refactor app_api folder: Add test_tools.sql imports and simplify data setup
    Refactored 55 test files in the app_api folder to use centralized
    test utilities from test_tools.sql instead of verbose INSERT statements.
    
    Changes:
    - Added `\ir ../test_tools.sql` import to all refactored tests
    - Replaced INSERT INTO hafd.blocks with test.create_blocks()
    - Replaced INSERT INTO hafd.operation_types with test.create_operation_types()
    - Kept hafd.accounts INSERTs as they require test-specific data
    
    Skipped 37 tests that use blocks_reversible tables due to complex
    FK dependencies that require blocks to exist before reversible data.
    
    All 113 app_api tests pass after refactoring.

--- tests/integration/functional/hive_fork_manager/app_api/app_set_current_block_number.sql
+++ tests/integration/functional/hive_fork_manager/app_api/app_set_current_block_number.sql
@@ -1,14 +1,12 @@
+-- Load test utilities
+\ir ../test_tools.sql
 
 CREATE OR REPLACE PROCEDURE haf_admin_test_given()
         LANGUAGE 'plpgsql'
 AS
 $BODY$
 BEGIN
-    INSERT INTO hafd.blocks
-    VALUES
-          ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-        , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:24-07'::timestamp, 6, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )
-    ;
+    PERFORM test.create_blocks(1, 2);
 
     INSERT INTO hafd.accounts( id, name, block_num )
     VALUES (5, 'initminer', 1)
