2a8b189126bd1747c3c1f7c47808cfe729a0b93e
Krzysztof Leśniak <klesniak@syncad.com>
2025-12-15 18:30:16 +0100

    Add test for nonexistent table

--- tests/integration/functional/hive_fork_manager/app_api/vacuum_request.sql
+++ tests/integration/functional/hive_fork_manager/app_api/vacuum_request.sql
@@ -35,3 +35,36 @@ BEGIN
 END;
 $BODY$
 ;
+
+CREATE OR REPLACE PROCEDURE alice_test_nonexistent_table()
+    LANGUAGE 'plpgsql'
+AS
+$BODY$
+DECLARE
+    __error_caught BOOLEAN := FALSE;
+    __error_message TEXT;
+BEGIN
+    -- Try to request vacuum on non-existent table
+    BEGIN
+        PERFORM hive.app_request_table_vacuum('a', 'nonexistent_table');
+
+        -- If we get here, validation failed
+        RAISE EXCEPTION 'Expected exception for non-existent table but none was raised';
+    EXCEPTION
+        WHEN SQLSTATE '42P01' THEN
+            __error_caught := TRUE;
+            __error_message := SQLERRM;
+            RAISE NOTICE 'Correctly caught exception for non-existent table: %', __error_message;
+    END;
+
+    ASSERT __error_caught,
+        'Should raise exception when requesting vacuum on non-existent table';
+
+    -- Verify no request was created for the non-existent table
+    ASSERT (SELECT COUNT(*) FROM hafd.vacuum_requests WHERE schema_name = 'a' AND table_name = 'nonexistent_table') = 0,
+        'No vacuum request should be created for non-existent table';
+
+    RAISE NOTICE 'Test passed: non-existent table correctly rejected';
+END;
+$BODY$
+;
