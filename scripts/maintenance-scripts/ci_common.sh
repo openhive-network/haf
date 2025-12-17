04344d14cfb7146b9355499b0145126a1e1ced3f
Dan Notestein <dan@syncad.com>
2025-12-16 21:08:30 -0500

    Fix OP_BODY_FILTER_PATH to use dynamic REPO_DIR instead of hardcoded path
    The hardcoded /builds/hive/haf/ path doesn't work with GitLab runners
    that use unique build directories like /builds/MQ9am3W0k/0/hive/haf/.
    Use REPO_DIR which is already computed correctly from the script location.

--- scripts/maintenance-scripts/ci_common.sh
+++ scripts/maintenance-scripts/ci_common.sh
@@ -11,7 +11,7 @@ export COMPRESS_BLOCK_LOG_PATH=${COMPRESS_BLOCK_LOG_PATH:-"/home/hived/bin/compr
 export BLOCK_LOG_UTIL_PATH=${BLOCK_LOG_UTIL_PATH:-"/home/hived/bin/block_log_util"}
 export GET_DEV_KEY_PATH=${GET_DEV_KEY_PATH:-"/home/hived/bin/get_dev_key"}
 export CLI_WALLET_PATH=${CLI_WALLET_PATH:-"/home/hived/bin/cli_wallet"}
-export OP_BODY_FILTER_PATH=${OP_BODY_FILTER_PATH:-"/builds/hive/haf/haf-testnet-binaries/op_body_filter"}
+export OP_BODY_FILTER_PATH=${OP_BODY_FILTER_PATH:-"${REPO_DIR}haf-testnet-binaries/op_body_filter"}
 # export OP_BODY_FILTER_PATH=${OP_BODY_FILTER_PATH:-"${HAF_SOURCE_DIR:?"HAF source directory must be set"}/../build/bin/op_body_filter"}
 
 export DB_NAME=haf_block_log
