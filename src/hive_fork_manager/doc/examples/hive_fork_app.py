b12ec2c8cfeee832105d334e62990a1465730709
Dan Notestein <dan@syncad.com>
2025-12-29 00:58:59 -0500

    Add commit() after prepare_application_data for SQLAlchemy 2.x
    SQLAlchemy 2.x auto-begins transactions on connect(). Need to commit
    the initial transaction before main_loop() can call begin().

--- src/hive_fork_manager/doc/examples/hive_fork_app.py
+++ src/hive_fork_manager/doc/examples/hive_fork_app.py
@@ -96,6 +96,7 @@ def start_application(db_name, pg_port):
     engine = create_db_engine(db_name, pg_port)
     with engine.connect() as db_connection:
         prepare_application_data( db_connection )
+        db_connection.commit()
         main_loop( db_connection )
 
 if __name__ == '__main__':
