2a1c469332148ad27bc5815dfb39ebde5e59dcd4
Mateusz Kudela <mkudela@syncad.com>
2026-01-20 13:37:50 +0000

    Update hive submodule for test-tools wheel changes
    - Update hive submodule to mkudela/tt-wheel-changes branch (f554a3b74)
      which replaces test-tools submodule with wheel from GitLab Package Registry
    - Remove test-tools submodule references from .gitlab-ci.yml:
      - Remove from git submodule update --init command
      - Update comments to reflect test-tools is no longer a submodule
    - Update documentation link in monolithic_workaround.py to point to
      GitLab repository instead of removed submodule path

--- tests/integration/haf-local-tools/haf_local_tools/haf_node/monolithic_workaround.py
+++ tests/integration/haf-local-tools/haf_local_tools/haf_node/monolithic_workaround.py
@@ -6,5 +6,5 @@ if TYPE_CHECKING:
     from test_tools.__private.user_handles.handles.node_handles.runnable_node_handle import RunnableNodeHandle
 
 def apply_block_log_type_to_monolithic_workaround(node: RunnableNodeHandle) -> None:
-    # MORE INFO: hive/tests/python/hive-local-tools/test-tools/package/test_tools/__private/node.py (class Node, block_log property)
+    # MORE INFO: https://gitlab.syncad.com/hive/test-tools/-/blob/3b0a51cb93802e02a2fd3643c6b7d801c07acbc7/package/test_tools/__private/node.py#L141
     node.config.block_log_split = -1
