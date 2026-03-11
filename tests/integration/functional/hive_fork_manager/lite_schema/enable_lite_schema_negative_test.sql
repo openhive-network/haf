ca188280143b5557169a39ff6952bd07c4c4b79d
Dan Notestein <dan@syncad.com>
2026-03-11 04:30:00 -0400

    Fix producer account in negative test for deferred FK check
    Include a producer account in push_block to satisfy the deferred FK
    on blocks.producer_account_id when set_irreversible copies to
    irreversible tables.

--- tests/integration/functional/hive_fork_manager/lite_schema/enable_lite_schema_negative_test.sql
+++ tests/integration/functional/hive_fork_manager/lite_schema/enable_lite_schema_negative_test.sql
@@ -12,13 +12,13 @@ BEGIN
     -- Connect in full mode first
     PERFORM hive.connect( 'test_full', 0, 0, 0, FALSE, FALSE );
 
-    -- Push a block to get data in the DB
+    -- Push a block to get data in the DB (include a producer account to satisfy FK)
     PERFORM hive.push_block(
-          ( 1, '\xBADD', '\xCAFE', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65wH1LZ7BfSHcK69SShnqCAH5xdoSZpGkUjmzHJ5GCuxEK9V5G', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )::hafd.blocks
+          ( 1, '\xBADD', '\xCAFE', '2016-06-22 19:10:25-07'::timestamp, 1, '\x4007', E'[]', '\x2157', 'STM65wH1LZ7BfSHcK69SShnqCAH5xdoSZpGkUjmzHJ5GCuxEK9V5G', 1000, 1000, 1000000, 1000, 1000, 1000, 2000, 2000 )::hafd.blocks
         , ARRAY[]::hafd.transactions[]
         , ARRAY[]::hafd.transactions_multisig[]
         , ARRAY[]::hafd.operations[]
-        , ARRAY[]::hafd.accounts[]
+        , ARRAY[ ROW(1, 'producer', 1)::hafd.accounts ]
         , ARRAY[]::hafd.account_operations[]
         , ARRAY[]::hafd.applied_hardforks[]
     );
