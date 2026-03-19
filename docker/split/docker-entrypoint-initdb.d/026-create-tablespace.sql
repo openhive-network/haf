1b4056918a9f47f774e925558057162565a994c3
Eric Frias <efrias@syncad.com>
2026-03-19 18:58:06 -0400

    Fix CREATE TABLESPACE: cannot be executed from a function

--- docker/split/docker-entrypoint-initdb.d/026-create-tablespace.sql
+++ docker/split/docker-entrypoint-initdb.d/026-create-tablespace.sql
@@ -1,10 +1,4 @@
 -- Create the HAF tablespace for storing database data.
 -- The directory /var/lib/postgresql/tablespace is created by the entrypoint.
-
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_tablespace WHERE spcname = 'haf_tablespace') THEN
-        CREATE TABLESPACE haf_tablespace OWNER haf_admin LOCATION '/var/lib/postgresql/tablespace';
-    END IF;
-END
-$$;
+-- This only runs on first boot, so the tablespace won't already exist.
+CREATE TABLESPACE haf_tablespace OWNER haf_admin LOCATION '/var/lib/postgresql/tablespace';
