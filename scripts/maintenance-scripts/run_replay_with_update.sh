90a35455ce92a360fc2a316c7d5a19a527a5f6ad
asuch <asuch@syncad.com>
2026-04-09 12:24:45 +0200

    Fix replay_and_update job - it should use postgres 18 and gcc14

--- scripts/maintenance-scripts/run_replay_with_update.sh
+++ scripts/maintenance-scripts/run_replay_with_update.sh
@@ -2,9 +2,12 @@
 set -xeuo pipefail
 
 sudo apt-get update
-POSTGRES_VERSION="${POSTGRES_VERSION:-17}"
-sudo apt-get -y install git cmake ninja-build build-essential liburing-dev libboost-all-dev libssl-dev bzip2 libbz2-dev libsnappy-dev \
+POSTGRES_VERSION="${POSTGRES_VERSION:-18}"
+sudo apt-get -y install git cmake ninja-build build-essential gcc-14 g++-14 liburing-dev libboost-all-dev libssl-dev bzip2 libbz2-dev libsnappy-dev \
   python3-jinja2 libreadline-dev postgresql-server-dev-${POSTGRES_VERSION} zopfli libpqxx-dev
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SCRIPTSDIR="$SCRIPTPATH/.."
