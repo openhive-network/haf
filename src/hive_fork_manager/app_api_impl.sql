openhive-network/haf

commit 3242af8a37040656f0e79bb65ccc2ddca55b80f9

extract attachment from hive.contexts to hive.contexts_attachment
There was a problem with starving removing reversible data
because in some rare situations lock was taken to often and
prevented 'set_irreversible' function to clean up obsolete blocks.
To ensure that lock is held only when attachment was changed, column
is_attached was moved to separated table.

Problem was obserwed in system test 'replay_with_app'. In the test log
there was iformation about minimal reversible block, which was few
tousands behind the head block;

src/hive_fork_manager/app_api_impl.sql
@@ -361,9 +361,10 @@ $BODY$
 
         SELECT
                hac.current_block_num
-             , hac.is_attached
+             , hca.is_attached
              , hac.irreversible_block
         FROM hive.contexts hac
+        JOIN hive.contexts_attachment hca ON hca.context_id = hac.id
         WHERE hac.name = __lead_context
         INTO __context_state;
 

src/hive_fork_manager/hived_api_impl.sql
@@ -258,7 +258,8 @@ BEGIN
          , COALESCE( min(irreversible_block), __lowest_irreversible_block )
     INTO __min_ctx_fork_id, __lowest_irreversible_block
     FROM hive.contexts hc
-    WHERE hc.is_attached = TRUE
+    JOIN hive.contexts_attachment hca ON hca.context_id = hc.id
+    WHERE hca.is_attached = TRUE
     AND hc.is_forking = TRUE;
 
     __max_block_num := LEAST(__lowest_irreversible_block, _new_irreversible_block);

src/hive_fork_manager/app_api.sql
@@ -189,7 +189,7 @@ BEGIN
     -- lock EXCLUSIVE may be taken by hived in function:
     -- hive.set_irreversible
     -- so here we can stuck while hived is servicing a new irreversible block notification
-    LOCK TABLE hive.contexts IN SHARE MODE;
+    LOCK TABLE hive.contexts_attachment IN SHARE MODE;
 
     SELECT hc.current_block_num INTO __current_block_num
     FROM hive.contexts hc
@@ -472,8 +472,9 @@ DECLARE
 BEGIN
     PERFORM hive.app_check_contexts_synchronized( _contexts );
 
-    SELECT ARRAY_AGG( DISTINCT(hc.is_attached) )  is_attached INTO __result
-    FROM hive.contexts hc
+    SELECT ARRAY_AGG( DISTINCT(hca.is_attached) )  is_attached INTO __result
+    FROM hive.contexts_attachment hca
+    JOIN hive.contexts hc ON hc.id = hca.context_id
     WHERE hc.name =ANY( _contexts );
 
     IF __result IS NULL OR ARRAY_LENGTH( __result, 1 ) != 1 THEN
@@ -543,8 +544,9 @@ BEGIN
     PERFORM hive.app_check_contexts_synchronized( _contexts );
 
     SELECT ARRAY_AGG(id) INTO __contexts_id
-    FROM hive.contexts
-    WHERE name = ANY( _contexts ) AND is_attached = FALSE;
+    FROM hive.contexts hc
+    JOIN hive.contexts_attachment hca ON hca.context_id = hc.id
+    WHERE name = ANY( _contexts ) AND hca.is_attached = FALSE;
 
     IF __contexts_id IS NULL THEN
         RAISE EXCEPTION 'Contexts do not exist';
@@ -683,8 +685,9 @@ DECLARE
     __is_attached BOOL;
     __current_block_num hive.blocks.num%TYPE;
 BEGIN
-    SELECT hac.id, hac.is_attached, hac.current_block_num
+    SELECT hac.id, hca.is_attached, hac.current_block_num
     FROM hive.contexts hac
+    JOIN hive.contexts_attachment hca ON hac.id = hca.context_id
     WHERE hac.name = _context
         INTO __context_id, __is_attached, __current_block_num;
 
@@ -783,7 +786,7 @@ BEGIN
     SELECT COUNT(
         DISTINCT(
                    ctx.current_block_num
-                 , ctx.is_attached
+                 , hca.is_attached
                  , ctx.events_id
                  , ctx.is_forking
                  , (ctx.loop).size_of_blocks_batch
@@ -792,6 +795,7 @@ BEGIN
         )
     ) INTO __number_of_rows
     FROM hive.contexts ctx
+    JOIN hive.contexts_attachment hca ON hca.context_id = ctx.id
     WHERE ctx.name =ANY(_contexts);
 
     IF __number_of_rows != 1 THEN
@@ -807,9 +811,10 @@ CREATE OR REPLACE FUNCTION hive.is_app_in_sync( _contexts hive.contexts_group  )
 AS
 $BODY$
 BEGIN
-    RETURN COALESCE((SELECT BOOL_AND(hive.contexts.id IS NOT NULL AND is_attached AND consistent_block - current_block_num <= 1)
+    RETURN COALESCE((SELECT BOOL_AND(hc.id IS NOT NULL AND hca.is_attached AND consistent_block - hc.current_block_num <= 1)
                      FROM UNNEST(_contexts) AS context_names(name)
-                     LEFT JOIN hive.contexts USING(name)
+                     LEFT JOIN hive.contexts hc USING(name)
+                     JOIN hive.contexts_attachment hca ON hca.context_id = hc.id
                      CROSS JOIN hive.irreversible_data), FALSE);
 END;
 $BODY$

src/hive_fork_manager/authorization.sql
@@ -60,6 +60,7 @@ GRANT ALL ON ALL SEQUENCES IN SCHEMA hive TO hived_group, hive_applications_grou
 GRANT ALL ON  ALL TABLES IN SCHEMA hive TO hived_group;
 GRANT SELECT ON ALL TABLES IN SCHEMA hive TO hive_applications_group;
 GRANT ALL ON hive.contexts TO hive_applications_group;
+GRANT ALL ON hive.contexts_attachment TO hive_applications_group;
 GRANT ALL ON hive.registered_tables TO hive_applications_group;
 GRANT ALL ON hive.triggers TO hive_applications_group;
 GRANT ALL ON hive.state_providers_registered TO hive_applications_group;
@@ -68,20 +69,38 @@ GRANT ALL ON hive.state_providers_registered TO hive_applications_group;
 REVOKE UPDATE( is_forking, owner ) ON hive.contexts FROM GROUP hive_applications_group;
 ALTER TABLE hive.contexts ENABLE ROW LEVEL SECURITY;
 
+REVOKE UPDATE( owner ) ON hive.contexts_attachment FROM GROUP hive_applications_group;
+ALTER TABLE hive.contexts_attachment ENABLE ROW LEVEL SECURITY;
+
 DROP POLICY IF EXISTS dp_hive_context ON hive.contexts CASCADE;
 CREATE POLICY dp_hive_context ON hive.contexts FOR INSERT WITH CHECK ( current_user = owner );
+
+DROP POLICY IF EXISTS dp_hive_contexts_attachment ON hive.contexts_attachment CASCADE;
+CREATE POLICY dp_hive_contexts_attachment ON hive.contexts_attachment FOR INSERT WITH CHECK ( current_user = owner );
 
 DROP POLICY IF EXISTS sp_hived_hive_context ON hive.contexts CASCADE;
 CREATE POLICY sp_hived_hive_context ON hive.contexts FOR SELECT TO hived_group USING( TRUE );
 
+DROP POLICY IF EXISTS sp_hived_hive_contexts_attachment ON hive.contexts_attachment CASCADE;
+CREATE POLICY sp_hived_hive_contexts_attachment ON hive.contexts_attachment FOR SELECT TO hived_group USING( TRUE );
+
 DROP POLICY IF EXISTS sp_applications_hive_context ON hive.contexts CASCADE;
 CREATE POLICY sp_applications_hive_context ON hive.contexts FOR SELECT TO hive_applications_group USING( TRUE );
+
+DROP POLICY IF EXISTS sp_applications_hive_contexts_attachment ON hive.contexts_attachment CASCADE;
+CREATE POLICY sp_applications_hive_contexts_attachment ON hive.contexts_attachment FOR SELECT TO hive_applications_group USING( TRUE );
 
 DROP POLICY IF EXISTS sp_applications_update_hive_context ON hive.contexts CASCADE;
 CREATE POLICY sp_applications_update_hive_context ON hive.contexts FOR UPDATE TO hive_applications_group USING( TRUE ) WITH CHECK( hive.can_impersonate(current_user, owner) ) ;
 
+DROP POLICY IF EXISTS sp_applications_update_hive_contexts_attachment ON hive.contexts_attachment CASCADE;
+CREATE POLICY sp_applications_update_hive_contexts_attachment ON hive.contexts_attachment FOR UPDATE TO hive_applications_group USING( TRUE ) WITH CHECK( hive.can_impersonate(current_user, owner) ) ;
+
 DROP POLICY IF EXISTS sp_applications_delete_hive_context ON hive.contexts CASCADE;
 CREATE POLICY sp_applications_delete_hive_context ON hive.contexts FOR DELETE TO hive_applications_group USING( hive.can_impersonate(current_user, owner) );
+
+DROP POLICY IF EXISTS sp_applications_delete_hive_contexts_attachment ON hive.contexts_attachment CASCADE;
+CREATE POLICY sp_applications_delete_hive_contexts_attachment ON hive.contexts_attachment FOR DELETE TO hive_applications_group USING( hive.can_impersonate(current_user, owner) );
 
 DROP POLICY IF EXISTS sp_applications_hive_state_providers ON hive.state_providers_registered CASCADE;
 CREATE POLICY sp_applications_hive_state_providers ON hive.state_providers_registered FOR SELECT TO hive_applications_group USING( hive.can_impersonate(current_user, owner) );

src/hive_fork_manager/blocks_views_for_contexts.sql
@@ -15,7 +15,7 @@ EXECUTE format(
         SELECT
         hc.current_block_num,
         hc.irreversible_block,
-        hc.is_attached,
+        hca.is_attached,
         hc.fork_id,
         /*
             Definition of `min_block` (from least(current_block_num, irrecersible_block)) has been changed because of creation of gap,
@@ -29,6 +29,7 @@ EXECUTE format(
         ) AS min_block,
         hc.current_block_num > hc.irreversible_block AND hc.is_forking AS reversible_range
         FROM hive.contexts hc
+        JOIN hive.contexts_attachment hca ON hc.id = hca.context_id
         WHERE hc.name::text = ''%s''::text
         limit 1
         ;', __schema, _context_name

src/hive_fork_manager/context_rewind/data_schema.sql
@@ -13,7 +13,6 @@ CREATE TABLE IF NOT EXISTS hive.contexts(
     schema TEXT NOT NULL,
     current_block_num INTEGER NOT NULL,
     irreversible_block INTEGER NOT NULL,
-    is_attached BOOL NOT NULL,
     back_from_fork BOOL NOT NULL DEFAULT FALSE,
     events_id BIGINT NOT NULL DEFAULT 0, -- 0 - is a special fake event, means no events are processed, it is required to satisfy FK constraint
     fork_id BIGINT NOT NULL DEFAULT 1,
@@ -31,6 +30,16 @@ SELECT pg_catalog.pg_extension_config_dump('hive.contexts', '');
 SELECT pg_catalog.pg_extension_config_dump('hive.contexts_id_seq', '');
 
 CREATE INDEX IF NOT EXISTS hive_contexts_owner_idx ON hive.contexts( owner );
+
+CREATE TABLE IF NOT EXISTS hive.contexts_attachment(
+      context_id INTEGER NOT NULL UNIQUE
+    , is_attached BOOL NOT NULL
+    , owner NAME NOT NULL
+    , CONSTRAINT fk_contexts_attachment_context FOREIGN KEY(context_id) REFERENCES hive.contexts( id )
+);
+SELECT pg_catalog.pg_extension_config_dump('hive.contexts_attachment', '');
+
+CREATE INDEX IF NOT EXISTS hive_contexts_attachment_owner_idx ON hive.contexts_attachment( owner );
 
 CREATE TABLE IF NOT EXISTS hive.registered_tables(
    id SERIAL NOT NULL,

src/hive_fork_manager/context_rewind/detach_table.sql
@@ -84,10 +84,11 @@ BEGIN
     SELECT hrt.id, hrt.shadow_table_name, hc.is_forking
     FROM hive.registered_tables hrt
     JOIN hive.contexts hc ON hc.id = hrt.context_id
+    JOIN hive.contexts_attachment hca ON hc.id = hca.context_id
     WHERE
           hrt.origin_table_schema = lower( _table_schema )
       AND hrt.origin_table_name = _table_name
-      AND hc.is_attached = FALSE
+      AND hca.is_attached = FALSE
     INTO __table_id, __shadow_table_name, __context_is_forking;
 
     IF __table_id IS NULL THEN

src/hive_fork_manager/context_rewind/register_table.sql
@@ -205,15 +205,15 @@ BEGIN
     SELECT hive.create_shadow_table(  _table_schema, _table_name ) INTO  __shadow_table_name;
 
     -- insert information about new registered table
-    INSERT INTO hive.registered_tables( context_id, origin_table_schema, origin_table_name, shadow_table_name, origin_table_columns, owner )
+    INSERT INTO hive.registered_tables as hrt( context_id, origin_table_schema, origin_table_name, shadow_table_name, origin_table_columns, owner )
     SELECT hc.id, tables.table_schema, tables.origin, tables.shadow, columns, current_user
     FROM ( SELECT hc.id, hive.check_owner( hc.name, hc.owner ) FROM hive.contexts hc WHERE hc.name =  _context_name ) as hc
     JOIN ( VALUES( lower(_table_schema), lower(_table_name), __shadow_table_name, __columns_names  )  ) as tables( table_schema, origin, shadow, columns ) ON TRUE
     RETURNING
           context_id
         , id
-        , (SELECT hc2.is_attached FROM hive.contexts hc2 WHERE hc2.id = context_id)
-        , (SELECT hc2.is_forking FROM hive.contexts hc2 WHERE hc2.id = context_id)
+        , (SELECT hca.is_attached FROM hive.contexts hc2 JOIN hive.contexts_attachment hca ON hca.context_id=hc2.id WHERE hc2.id = hrt.context_id)
+        , (SELECT hc2.is_forking FROM hive.contexts hc2 WHERE hc2.id = hrt.context_id)
         INTO __context_id, __registered_table_id, __attached_context, __is_forking
     ;
 

commit 649eb9fee212aea64fa9e4e051a2704f19020220

Fix FK violation in end_massive_sync during pruned REINDEX with concurrent app
When pruning is enabled, applications are not blocked during REINDEX
(is_instance_ready() returns TRUE via is_pruning_enabled() fast path).
The app's squash_and_get_state runs concurrently with hived's
end_massive_sync, creating a race: squash_end_massive_sync_events moves
contexts.events_id backwards while remove_unecessary_events reads
MIN(events_id) and deletes events, causing an FK violation.

This is a regression from 3242af8a3 (Jun 2024) which changed the lock
from ACCESS EXCLUSIVE on hafd.contexts to EXCLUSIVE on
hafd.contexts_attachment. The new lock blocks app_context_attach but
not the squash/find_next_event functions that also modify events_id.

Fix: add LOCK TABLE hafd.contexts_attachment IN ROW SHARE MODE in
squash_and_get_state(), restoring mutual exclusion with hived's
EXCLUSIVE lock without reintroducing the starvation problem.

src/hive_fork_manager/app_api_impl.sql
@@ -370,6 +370,13 @@ $BODY$
         __lead_context hafd.context_name := _contexts[ 1 ];
         __hive_sync_state hafd.sync_state;
     BEGIN
+        -- Coordinate with hived's event cleanup (end_massive_sync / set_irreversible),
+        -- which takes EXCLUSIVE lock on contexts_attachment. ROW SHARE here ensures
+        -- no events are deleted while we're modifying contexts.events_id in squash
+        -- functions or find_next_event. ROW SHARE locks are compatible with each other,
+        -- so multiple applications can process events concurrently.
+        LOCK TABLE hafd.contexts_attachment IN ROW SHARE MODE;
+
         PERFORM hive.squash_events( _contexts );
 
         SELECT hive.get_sync_state() INTO __hive_sync_state;

src/hive_fork_manager/hived_api_impl.sql
@@ -330,19 +330,10 @@ BEGIN
     FROM hafd.events_queue heq
     WHERE heq.event != 'BACK_FROM_FORK' AND heq.block_num = ( _new_irreversible_block + 1 ); --next block after irreversible
 
-    -- You may think that SELECT FOR UPDATE needs to be used here in USING clause
-    -- but SELECT FOR UPDATE will lock hafd.contexts, so it want to acquire lock
-    -- between hived and application, and if application will modify contexts and never commit (by mistake or maliciously)
-    -- then hived will be locked forever
-    --
-    -- Important notice from the pg documentation https://www.postgresql.org/docs/current/transaction-iso.html :
-    -- UPDATE, DELETE, SELECT FOR UPDATE, and SELECT FOR SHARE commands behave the same as SELECT in terms of searching
-    -- for target rows: they will only find target rows that were committed as of the command start time. However, such
-    -- a target row might have already been updated (or deleted or locked) by another concurrent transaction by the
-    -- time it is found.
-    --
-    -- It means that SELECT from USING clause will return min event = 10, but in case of a bug an application
-    -- context may back to event 9 and then when DELETE is being committed it will violate FK(event_queue(id)<->contexts(events_id))
+    -- The caller (end_massive_sync / set_irreversible) holds EXCLUSIVE lock on
+    -- hafd.contexts_attachment, which blocks any concurrent squash_and_get_state
+    -- (that holds ROW SHARE on the same table). This ensures no application can
+    -- modify contexts.events_id while we read MIN(events_id) and delete events.
 
     DELETE FROM hafd.events_queue heq
     USING ( SELECT MIN( hc.events_id) as id FROM hafd.contexts hc ) as min_event